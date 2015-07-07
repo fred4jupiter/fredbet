c82e7dfdb9169ab37dfd351802a99c974b838096
Staehler, Michael <michael.staehler@opitz-consulting.com>
2015-07-07 22:03:38 +0200

    initial commit

---
+++ src/main/java/de/fred4jupiter/fredbet/ApplicationConfig.java
@@ -0,0 +1,12 @@
+package de.fred4jupiter.fredbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationConfig.class, args);
+    }
+}
---
+++ src/main/java/de/fred4jupiter/fredbet/web/IndexController.java
@@ -0,0 +1,13 @@
+package de.fred4jupiter.fredbet.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//@Controller
+public class IndexController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+}
