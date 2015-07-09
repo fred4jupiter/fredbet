332a4bc0d8fd95b63b51982baf70055cadeee5a1
Staehler, Michael <michael.staehler@opitz-consulting.com>
2015-07-09 22:15:55 +0200

    update layout

--- src/main/java/de/fred4jupiter/fredbet/WebSecurityConfig.java
+++ src/main/java/de/fred4jupiter/fredbet/WebSecurityConfig.java
@@ -13,8 +13,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/home", "/webjars/**").permitAll().anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").permitAll().and().logout().permitAll();
+		http.authorizeRequests().antMatchers("/", "/home", "/webjars/**").permitAll().anyRequest().authenticated();
+		http.formLogin().loginPage("/login").permitAll().and().logout().permitAll();
 	}
 
 	@Autowired
