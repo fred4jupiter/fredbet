cce8cc2bffc84775877b62cfcc65f9d8de7ae2c3
fred4jupiter <hamsterhase@gmx.de>
2015-07-24 20:13:58 +0200

    added icons and comment

--- src/main/java/de/fred4jupiter/fredbet/Application.java
+++ src/main/java/de/fred4jupiter/fredbet/Application.java
@@ -3,6 +3,14 @@ package de.fred4jupiter.fredbet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application start.
+ * 
+ * NOTE: To disable thymeleaf caching correctly please activate the dev profile on local development.
+ * 
+ * @author michael
+ *
+ */
 @SpringBootApplication
 public class Application {
 
