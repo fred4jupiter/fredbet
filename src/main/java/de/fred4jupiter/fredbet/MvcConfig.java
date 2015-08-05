efaff597c5c67147fad1d77e1ae628bd4efdeaa4
Staehler, Michael <michael.staehler@opitz-consulting.com>
2015-08-05 22:14:02 +0200

    handle active page state

--- src/main/java/de/fred4jupiter/fredbet/MvcConfig.java
+++ src/main/java/de/fred4jupiter/fredbet/MvcConfig.java
@@ -1,8 +1,11 @@
 package de.fred4jupiter.fredbet;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import de.fred4jupiter.fredbet.web.ActivePageHandlerInterceptor;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
@@ -11,6 +14,10 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("matches");
 		registry.addViewController("/login").setViewName("login");
-
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new ActivePageHandlerInterceptor());
 	}
 }
---
+++ src/main/java/de/fred4jupiter/fredbet/web/ActivePageHandlerInterceptor.java
@@ -0,0 +1,47 @@
+package de.fred4jupiter.fredbet.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class ActivePageHandlerInterceptor implements HandlerInterceptor {
+
+	private static final Logger LOG = LoggerFactory.getLogger(ActivePageHandlerInterceptor.class);
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView)
+			throws Exception {
+		String requestURI = request.getRequestURI();
+		LOG.info("requestURI: " + requestURI);
+
+		if (requestURI.contains("matches")) {
+			addStateAttributeFor("matches", modelAndView);
+		} else if (requestURI.contains("bet/open")) {
+			addStateAttributeFor("bet_open", modelAndView);
+		} else if (requestURI.contains("bet")) {
+			addStateAttributeFor("bet", modelAndView);
+		} else if (requestURI.contains("ranking")) {
+			addStateAttributeFor("ranking", modelAndView);
+		} else if (requestURI.contains("user")) {
+			addStateAttributeFor("user", modelAndView);
+		}
+	}
+
+	private void addStateAttributeFor(String page, ModelAndView modelAndView) {
+		modelAndView.addObject("pageState_" + page, "active");
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		return true;
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+	}
+
+}
