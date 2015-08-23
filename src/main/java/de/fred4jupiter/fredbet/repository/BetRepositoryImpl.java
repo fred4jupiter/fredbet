c68f1bd2e0649c2deb814ec783a924f9167163f1
fred4jupiter <hamsterhase@gmx.de>
2015-08-23 11:00:09 +0200

    added sorting

--- src/main/java/de/fred4jupiter/fredbet/repository/BetRepositoryImpl.java
+++ src/main/java/de/fred4jupiter/fredbet/repository/BetRepositoryImpl.java
@@ -3,10 +3,12 @@ package de.fred4jupiter.fredbet.repository;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.group;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregation;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.project;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.sort;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
@@ -20,7 +22,7 @@ class BetRepositoryImpl implements BetRepositoryCustom {
 
 	public List<UsernamePoints> calculateRanging() {
 		Aggregation aggregation = newAggregation(group("userName").sum("points").as("total"),
-				project("total").and("userName").previousOperation());
+				project("total").and("userName").previousOperation(), sort(Sort.Direction.DESC, "total"));
 
 		// Convert the aggregation result into a List
 		AggregationResults<UsernamePoints> aggregationResults = mongoTemplate.aggregate(aggregation, Bet.class, UsernamePoints.class);
