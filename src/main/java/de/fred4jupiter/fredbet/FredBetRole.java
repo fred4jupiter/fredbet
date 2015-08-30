00e3dce4f709df188332f0ecc2aee4ac41175878
fred4jupiter <hamsterhase@gmx.de>
2015-08-30 12:46:01 +0200

    refactoring

--- src/main/java/de/fred4jupiter/fredbet/FredBetRole.java
+++ src/main/java/de/fred4jupiter/fredbet/FredBetRole.java
@@ -1,14 +1,9 @@
 package de.fred4jupiter.fredbet;
 
-/**
- * NOTE: These are the roles used in FredBet application. The role prefix 'ROLE_' will be added by spring security automatically!!
- * 
- * @author MIS
- *
- */
 public enum FredBetRole {
 
 	ROLE_USER,
-	
+
 	ROLE_ADMIN,
+
 }
