cafd9bfb79a37acc962ee8afe053c5a2e13682ba
fred4jupiter <hamsterhase@gmx.de>
2015-08-30 13:37:37 +0200

    bugfix build info

--- src/main/java/de/fred4jupiter/fredbet/Application.java
+++ src/main/java/de/fred4jupiter/fredbet/Application.java
@@ -2,7 +2,6 @@ package de.fred4jupiter.fredbet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * Main application start.
@@ -13,7 +12,6 @@ import org.springframework.context.annotation.PropertySource;
  *
  */
 @SpringBootApplication
-//@PropertySource("classpath:/build-info.properties")
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/de/fred4jupiter/fredbet/web/BuildInfoController.java
+++ src/main/java/de/fred4jupiter/fredbet/web/BuildInfoController.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller("/buildinfo")
+//@Controller("/buildinfo")
 public class BuildInfoController {
 
 	@Autowired
