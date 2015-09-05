cfc3b87ceedd4ba4a5b8f1248c7319fe0e6b12cb
fred4jupiter <hamsterhase@gmx.de>
2015-09-05 15:21:31 +0200

    update default demo data

--- src/main/java/de/fred4jupiter/fredbet/data/DataBasePopulator.java
+++ src/main/java/de/fred4jupiter/fredbet/data/DataBasePopulator.java
@@ -15,7 +15,6 @@ import de.fred4jupiter.fredbet.domain.AppUser;
 import de.fred4jupiter.fredbet.domain.Group;
 import de.fred4jupiter.fredbet.domain.Match;
 import de.fred4jupiter.fredbet.domain.MatchBuilder;
-import de.fred4jupiter.fredbet.service.BettingService;
 import de.fred4jupiter.fredbet.service.MatchService;
 import de.fred4jupiter.fredbet.service.UserService;
 
@@ -33,30 +32,14 @@ public class DataBasePopulator {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private BettingService bettingService;
-
     @PostConstruct
     public void initDatabaseWithDemoData() {
         LOG.info("initDatabaseWithDemoData: inserting demo data...");
 
-        // will also be used for remote shell login
-        final AppUser adminUser = new AppUser("admin", "admin", FredBetRole.ROLE_USER, FredBetRole.ROLE_ADMIN, FredBetRole.ROLE_EDIT_MATCH);
-        userService.save(adminUser);
-        final AppUser editUser = new AppUser("edit", "edit", FredBetRole.ROLE_USER, FredBetRole.ROLE_EDIT_MATCH);
-        userService.save(editUser);
-        
-        final AppUser normalUser = new AppUser("normal", "normal", FredBetRole.ROLE_USER);
-        userService.save(normalUser);
+        createDefaultUsers();
 
         // this we be executed in demodata profile only
         if (environment.acceptsProfiles("demodata")) {
-
-            // for (int i = 0; i < 10; i++) {
-            // userService.save(new AppUser("test" + i, "test" + i,
-            // FredBetRole.ROLE_USER));
-            // }
-
             Match match1 = MatchBuilder.create().withTeams("Bulgarien", "Irland").withGroup(Group.GROUP_A)
                     .withStadium("Westfalenstadium, Dortmund").withKickOffDate(LocalDateTime.now().plusMinutes(5)).build();
             matchService.save(match1);
@@ -68,15 +51,15 @@ public class DataBasePopulator {
             Match match3 = MatchBuilder.create().withTeams("Belgien", "England").withGroup(Group.GROUP_D).withStadium("AOL Arena, München")
                     .withKickOffDate(LocalDateTime.now().plusMinutes(15)).build();
             matchService.save(match3);
-
-            bettingService.createAndSaveBetting(adminUser, match1, 2, 1);
-            bettingService.createAndSaveBetting(adminUser, match2, 3, 1);
-            bettingService.createAndSaveBetting(adminUser, match3, 2, 4);
-
-            bettingService.createAndSaveBetting(editUser, match1, 0, 1);
-            bettingService.createAndSaveBetting(editUser, match2, 1, 2);
-            bettingService.createAndSaveBetting(editUser, match3, 5, 0);
         }
+    }
+
+    private void createDefaultUsers() {
+        // will also be used for remote shell login
+        userService.save(new AppUser("admin", "admin", FredBetRole.ROLE_USER, FredBetRole.ROLE_ADMIN, FredBetRole.ROLE_EDIT_MATCH));
+        
+        userService.save(new AppUser("edit", "edit", FredBetRole.ROLE_USER, FredBetRole.ROLE_EDIT_MATCH));
+        userService.save(new AppUser("normal", "normal", FredBetRole.ROLE_USER));
     }
 
 }
