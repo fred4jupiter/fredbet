dc56601b00f9d2238a8526e7e2d2a00432a619eb
fred4jupiter <hamsterhase@gmx.de>
2015-12-13 19:13:52 +0100

    added real demo data

--- src/main/java/de/fred4jupiter/fredbet/data/DataBasePopulator.java
+++ src/main/java/de/fred4jupiter/fredbet/data/DataBasePopulator.java
@@ -22,52 +22,61 @@ import de.fred4jupiter.fredbet.service.UserService;
 @Component
 public class DataBasePopulator {
 
-	private static final Logger LOG = LoggerFactory.getLogger(DataBasePopulator.class);
+    private static final Logger LOG = LoggerFactory.getLogger(DataBasePopulator.class);
 
-	@Autowired
-	private MatchService matchService;
+    @Autowired
+    private MatchService matchService;
 
-	@Autowired
-	private Environment environment;
+    @Autowired
+    private Environment environment;
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@Autowired
-	private BettingService bettingService;
+    @Autowired
+    private BettingService bettingService;
 
-	@PostConstruct
-	public void initDatabaseWithDemoData() {
-		LOG.info("initDatabaseWithDemoData: inserting demo data...");
+    @PostConstruct
+    public void initDatabaseWithDemoData() {
+        LOG.info("initDatabaseWithDemoData: inserting demo data...");
 
-		createDefaultUsers();
+        createDefaultUsers();
 
-		// create matches for demo
-		if (environment.acceptsProfiles(FredBetProfile.DEMODATA)) {
-			bettingService.deleteAllBets();
-			matchService.deleteAllMatches();
+        // create matches for demo
+        if (environment.acceptsProfiles(FredBetProfile.DEMODATA)) {
+            bettingService.deleteAllBets();
+            matchService.deleteAllMatches();
 
-			matchService.save(MatchBuilder.create().withTeams("Bulgarien", "Irland").withGroup(Group.GROUP_A)
-					.withStadium("Westfalenstadium, Dortmund").withKickOffDate(LocalDateTime.now().plusMinutes(5)).build());
+            createMatches();
+        }
+    }
 
-			matchService.save(MatchBuilder.create().withTeams("Deutschland", "Frankfreich").withGroup(Group.GROUP_B)
-					.withStadium("Weserstadium, bremen").withKickOffDate(LocalDateTime.now().plusMinutes(10)).build());
+    private void createMatches() {
+        matchService.save(MatchBuilder.create().withTeams("Frankreich", "Rumänien").withGroup(Group.GROUP_A)
+                .withStadium("Saint-Denis").withKickOffDate(LocalDateTime.of(2016, 6, 10, 21, 0)).build());
 
-			matchService.save(MatchBuilder.create().withTeams("Belgien", "England").withGroup(Group.GROUP_D)
-					.withStadium("AOL Arena, München").withKickOffDate(LocalDateTime.now().plusMinutes(15)).build());
-		}
-	}
+        matchService.save(MatchBuilder.create().withTeams("Albanien", "Schweiz").withGroup(Group.GROUP_A)
+                .withStadium("Lens").withKickOffDate(LocalDateTime.of(2016, 6, 11, 15, 0)).build());
 
-	private void createDefaultUsers() {
-		// will also be used for remote shell login
-		userService.save(new AppUser("admin", "Pinky4Ever", false, FredBetRole.ROLE_USER, FredBetRole.ROLE_ADMIN, FredBetRole.ROLE_EDIT_MATCH));
-		userService.save(new AppUser("michael", "Pinky4Ever", FredBetRole.ROLE_USER, FredBetRole.ROLE_ADMIN, FredBetRole.ROLE_EDIT_MATCH));
+        matchService.save(MatchBuilder.create().withTeams("Wales", "Slowakei").withGroup(Group.GROUP_B)
+                .withStadium("Bordeaux").withKickOffDate(LocalDateTime.of(2016, 6, 11, 18, 0)).build());
 
-		userService.save(new AppUser("janz", "janz", FredBetRole.ROLE_USER, FredBetRole.ROLE_EDIT_MATCH));
-		userService.save(new AppUser("joernf", "joernf", FredBetRole.ROLE_USER, FredBetRole.ROLE_EDIT_MATCH));
+        matchService.save(MatchBuilder.create().withTeams("England", "Russland").withGroup(Group.GROUP_B)
+                .withStadium("Marseille").withKickOffDate(LocalDateTime.of(2016, 6, 11, 21, 0)).build());
 
-		userService.save(new AppUser("edit", "edit", FredBetRole.ROLE_USER, FredBetRole.ROLE_EDIT_MATCH));
-		userService.save(new AppUser("normal", "normal", FredBetRole.ROLE_USER));
-	}
+        matchService.save(MatchBuilder.create().withTeams("Türkei", "Kroatien").withGroup(Group.GROUP_D)
+                .withStadium("Paris").withKickOffDate(LocalDateTime.of(2016, 6, 12, 15, 0)).build());
+    }
 
+    private void createDefaultUsers() {
+        // will also be used for remote shell login
+        userService.save(new AppUser("admin", "Pinky4Ever", false, FredBetRole.ROLE_USER, FredBetRole.ROLE_ADMIN, FredBetRole.ROLE_EDIT_MATCH));
+        userService.save(new AppUser("michael", "Pinky4Ever", FredBetRole.ROLE_USER, FredBetRole.ROLE_ADMIN, FredBetRole.ROLE_EDIT_MATCH));
+
+        userService.save(new AppUser("janz", "janz", FredBetRole.ROLE_USER, FredBetRole.ROLE_EDIT_MATCH));
+        userService.save(new AppUser("joernf", "joernf", FredBetRole.ROLE_USER, FredBetRole.ROLE_EDIT_MATCH));
+
+        userService.save(new AppUser("edit", "edit", FredBetRole.ROLE_USER, FredBetRole.ROLE_EDIT_MATCH));
+        userService.save(new AppUser("normal", "normal", FredBetRole.ROLE_USER));
+    }
 }
