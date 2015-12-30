cf5735674e0752e4ec1e3691d405185199c351e6
fred4jupiter <hamsterhase@gmx.de>
2015-12-30 16:08:04 +0100

    update config

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}.$CIRCLE_BUILD_NUM versions:commit
-mvn build-helper:parse-version scm:tag -Dbasedir=. -Dtag=release_\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}.$CIRCLE_BUILD_NUM
+mvn build-helper:parse-version scm:tag -Dbasedir=. -Dtag=release_\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}.$CIRCLE_BUILD_NUM -Dusername=$GITHUB_USERNAME -Dpassword=$GITHUB_PASSWORD
 docker build -t fred4jupiter/fredbet .
 mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-SNAPSHOT versions:commit
 
