27b96a56aeef04fd0228f322550eb52a0c2110ad
fred4jupiter <hamsterhase@gmx.de>
2015-12-30 15:28:01 +0100

    update config

--- release.sh
+++ release.sh
@@ -4,3 +4,8 @@ mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorV
 mvn build-helper:parse-version scm:tag -Dbasedir=. -Dtag=release_\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}.$CIRCLE_BUILD_NUM -Dusername=$GITHUB_USERNAME -Dpassword=$GITHUB_PASSWORD
 docker build -t fred4jupiter/fredbet .
 mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-SNAPSHOT versions:commit
+
+# Deploy section
+docker login -e $TUTUM_EMAIL -u $TUTUM_USER -p $TUTUM_PASS  tutum.co
+docker tag -f fred4jupiter/fredbet tutum.co/fred4jupiter/fredbet:$CIRCLE_BUILD_NUM
+docker push tutum.co/fred4jupiter/fredbet:$CIRCLE_BUILD_NUM
