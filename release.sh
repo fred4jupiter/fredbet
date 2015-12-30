e6264664252c84a6cee2b00848a8eb678b4b6a58
fred4jupiter <hamsterhase@gmx.de>
2015-12-30 09:40:53 +0100

    update release script

--- release.sh
+++ release.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-mvn build-helper:parse-version versions:set -DnewVersion=${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion}.${CIRCLE_BUILD_NUM}
-mvn scm:tag -Dbasedir=. -Dtag=release_${project.version}
-mvn versions:revert
+CIRCLE_BUILD_NUM=1
+export CIRCLE_BUILD_NUM
+
+echo $CIRCLE_BUILD_NUM
+
+mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}.\$CIRCLE_BUILD_NUM versions:commit
+mvn scm:tag -Dbasedir=. -Dtag=release_\${project.version}
+docker build -t fred4jupiter/fredbet .
+mvn versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-SNAPSHOT
