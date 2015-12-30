4a8d2ded8eac61e1c447516a4cb31e8dcb5e063d
fred4jupiter <hamsterhase@gmx.de>
2015-12-30 15:01:53 +0100

    update pom and release script

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ export CIRCLE_BUILD_NUM
 
 echo $CIRCLE_BUILD_NUM
 
-mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}.\$CIRCLE_BUILD_NUM versions:commit
-mvn scm:tag -Dbasedir=. -Dtag=release_\${project.version}
+mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}.$CIRCLE_BUILD_NUM versions:commit
+mvn scm:tag -Dbasedir=. -Dtag=release_${project.version}
 docker build -t fred4jupiter/fredbet .
-mvn versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-SNAPSHOT
+mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-SNAPSHOT versions:commit
