6d51a398d92b8dfd5552f067bfbf3641be227ac5
fred4jupiter <hamsterhase@gmx.de>
2015-12-31 16:05:56 +0100

    added package for correct version in docker image

--- release.sh
+++ release.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# release section
 mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}.$CIRCLE_BUILD_NUM versions:commit
 mvn build-helper:parse-version scm:tag -Dbasedir=. -Dtag=release_\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}.$CIRCLE_BUILD_NUM -Dusername=$GITHUB_USERNAME -Dpassword=$GITHUB_PASSWORD
+mvn package
 docker build -t fred4jupiter/fredbet .
 mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-SNAPSHOT versions:commit
 
