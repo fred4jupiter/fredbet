797aa9c0f108c6d1143e864d765e3c85f23f3bce
fred4jupiter <hamsterhase@gmx.de>
2015-12-31 12:43:10 +0100

    added host address in infos

--- src/main/java/de/fred4jupiter/fredbet/web/BuildInfoController.java
+++ src/main/java/de/fred4jupiter/fredbet/web/BuildInfoController.java
@@ -1,11 +1,15 @@
 package de.fred4jupiter.fredbet.web;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.Properties;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,20 +19,40 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/buildinfo")
 public class BuildInfoController {
 
+	private static final Logger LOG = LoggerFactory.getLogger(BuildInfoController.class);
+	
 	@Autowired
 	private Properties buildProperties;
 
 	@RequestMapping
 	public ModelAndView list() {
 		ModelAndView modelAndView = new ModelAndView("buildinfo", "buildInfoMap", buildProperties);
+		addDynamicInfoProperties();
+		return modelAndView;
+	}
 
+	private void addDynamicInfoProperties() {
+		addCurrentDateTime();
+		addIPAddress();
+	}
+
+	private void addIPAddress() {
+		try {
+			InetAddress localHost = InetAddress.getLocalHost();
+			String hostAddress = localHost.getHostAddress();
+			buildProperties.put("hostAddress", hostAddress);
+		} catch (UnknownHostException e) {
+			LOG.error(e.getMessage(), e);
+		}
+	}
+
+	private void addCurrentDateTime() {
 		DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM, FormatStyle.MEDIUM);
 		DateTimeFormatter timeZoneFormatter = DateTimeFormatter.ofPattern("VV x");
-		
+
 		final String key = "currentDateTime";
-		final String value = formatter.format(LocalDateTime.now()) + ", "+timeZoneFormatter.format(ZonedDateTime.now());
-		
+		final String value = formatter.format(LocalDateTime.now()) + ", " + timeZoneFormatter.format(ZonedDateTime.now());
+
 		buildProperties.put(key, value);
-		return modelAndView;
 	}
 }
