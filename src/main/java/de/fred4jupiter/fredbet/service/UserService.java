b82c372b617a3e9533a9decb5987f3913ee69466
fred4jupiter <hamsterhase@gmx.de>
2016-01-16 16:35:38 +0100

    changed to radio button

--- src/main/java/de/fred4jupiter/fredbet/service/UserService.java
+++ src/main/java/de/fred4jupiter/fredbet/service/UserService.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import org.springframework.util.CollectionUtils;
 
-import de.fred4jupiter.fredbet.FredBetRole;
 import de.fred4jupiter.fredbet.domain.AppUser;
 import de.fred4jupiter.fredbet.domain.AppUserBuilder;
 import de.fred4jupiter.fredbet.repository.AppUserRepository;
