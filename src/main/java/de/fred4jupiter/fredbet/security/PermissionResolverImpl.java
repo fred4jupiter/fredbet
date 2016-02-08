adfbdfd7454f385472cc8d26bea06eacfc298144
fred4jupiter <hamsterhase@gmx.de>
2016-02-08 19:15:19 +0100

    added mapper for permission resolvin

--- src/main/java/de/fred4jupiter/fredbet/security/PermissionResolverImpl.java
+++ src/main/java/de/fred4jupiter/fredbet/security/PermissionResolverImpl.java
@@ -3,54 +3,30 @@ package de.fred4jupiter.fredbet.security;
 import java.util.Collection;
 import java.util.HashSet;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
 @Component
 public class PermissionResolverImpl implements PermissionResolver {
 
-    @Override
-    public Collection<GrantedAuthority> resolvePermissions(Collection<? extends GrantedAuthority> roleAuthorities) {
-        final Collection<GrantedAuthority> allPermissions = new HashSet<>();
-        
-        for (GrantedAuthority grantedAuthority : roleAuthorities) {
-            final String role = grantedAuthority.getAuthority();
-            Collection<? extends GrantedAuthority> permissionForRole = getPermissionForRole(role);
-            if (!CollectionUtils.isEmpty(permissionForRole)) {
-                allPermissions.addAll(permissionForRole);    
-            }
-        }
-        return allPermissions;
-    }
+	@Autowired
+	private RoleToPermissionMapper roleToPermissionMapper;
 
-    private Collection<? extends GrantedAuthority> getPermissionForRole(String role) {
-        final Collection<GrantedAuthority> permissionsForRole = new HashSet<>();
-        if (FredBetRole.ROLE_USER.name().equals(role)) {
-            return permissionsForRole;
-        }
-        
-        if (FredBetRole.ROLE_USER_ADVANCED.name().equals(role)) {
-            permissionsForRole.add(new SimpleGrantedAuthority(FredBetPermission.PERM_EDIT_MATCH));
-            return permissionsForRole;
-        }
-        
-        if (FredBetRole.ROLE_ADMIN.name().equals(role)) {
-            permissionsForRole.add(new SimpleGrantedAuthority(FredBetPermission.PERM_CREATE_MATCH));
-            permissionsForRole.add(new SimpleGrantedAuthority(FredBetPermission.PERM_EDIT_MATCH));
-            permissionsForRole.add(new SimpleGrantedAuthority(FredBetPermission.PERM_DELETE_MATCH));
+	@Override
+	public Collection<GrantedAuthority> resolvePermissions(Collection<? extends GrantedAuthority> roleAuthorities) {
+		final Collection<GrantedAuthority> allPermissions = new HashSet<>();
 
-            permissionsForRole.add(new SimpleGrantedAuthority(FredBetPermission.PERM_CREATE_USER));
-            permissionsForRole.add(new SimpleGrantedAuthority(FredBetPermission.PERM_DELETE_USER));
-            permissionsForRole.add(new SimpleGrantedAuthority(FredBetPermission.PERM_PASSWORD_RESET));
-            permissionsForRole.add(new SimpleGrantedAuthority(FredBetPermission.PERM_USER_ADMINISTRATION));
-            permissionsForRole.add(new SimpleGrantedAuthority(FredBetPermission.PERM_BUILD_INFO));
-            permissionsForRole.add(new SimpleGrantedAuthority(FredBetPermission.PERM_ADMINISTRATION));
-            return permissionsForRole;
-        }
-        
-        return null;
-    }
+		for (GrantedAuthority grantedAuthority : roleAuthorities) {
+			final String role = grantedAuthority.getAuthority();
+			Collection<? extends GrantedAuthority> permissionForRole = roleToPermissionMapper
+					.getPermissionForRole(FredBetRole.valueOf(role));
+			if (!CollectionUtils.isEmpty(permissionForRole)) {
+				allPermissions.addAll(permissionForRole);
+			}
+		}
+		return allPermissions;
+	}
 
 }
