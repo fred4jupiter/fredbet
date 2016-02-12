59c30ce71b2e927414950b1c065717e83d5385c7
fred4jupiter <hamsterhase@gmx.de>
2016-02-12 16:13:37 +0100

    update release script to tag and push to docker hub

--- release.sh
+++ release.sh
@@ -8,6 +8,6 @@ docker build -t fred4jupiter/fredbet .
 mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-SNAPSHOT versions:commit
 
 # Deploy section
-docker login -e $TUTUM_EMAIL -u $TUTUM_USER -p $TUTUM_PASS  tutum.co
-docker tag -f fred4jupiter/fredbet tutum.co/fred4jupiter/fredbet:$CIRCLE_BUILD_NUM
-docker push tutum.co/fred4jupiter/fredbet:$CIRCLE_BUILD_NUM
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker tag -f fred4jupiter/fredbet hamsterhase/fredbet:$CIRCLE_BUILD_NUM
+docker push hamsterhase/fredbet:$CIRCLE_BUILD_NUM
