50987c6873b744f19f01343bcc1a26464e158e2b
fred4jupiter <hamsterhase@gmx.de>
2016-02-13 09:10:37 +0100

    update docs

--- release.sh
+++ release.sh
@@ -7,9 +7,12 @@ mvn package
 docker build -t fred4jupiter/fredbet .
 mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-SNAPSHOT versions:commit
 
-# Deploy section
+# tag image and push to docker hub
 docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker tag -f fred4jupiter/fredbet hamsterhase/fredbet:$CIRCLE_BUILD_NUM
 docker push hamsterhase/fredbet:$CIRCLE_BUILD_NUM
 docker tag -f fred4jupiter/fredbet hamsterhase/fredbet:latest
 docker push hamsterhase/fredbet:latest
+
+# redeploy service in docker-cloud
+# docker-cloud service redeploy fredbet
