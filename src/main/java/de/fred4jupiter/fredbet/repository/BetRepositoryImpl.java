836572799e9ed039cd2a8f8489b33a5373c449ad
fred4jupiter <hamsterhase@gmx.de>
2016-02-14 11:15:24 +0100

    refactored into lambda

--- src/main/java/de/fred4jupiter/fredbet/repository/BetRepositoryImpl.java
+++ src/main/java/de/fred4jupiter/fredbet/repository/BetRepositoryImpl.java
@@ -1,30 +1,24 @@
 package de.fred4jupiter.fredbet.repository;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 class BetRepositoryImpl implements BetRepositoryCustom {
 
-	@Autowired
-	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
-	public List<UsernamePoints> calculateRanging() {
-		final String sql = "Select user_name, sum(points) as total from bet group by user_name order by total desc";
+    public List<UsernamePoints> calculateRanging() {
+        final String sql = "Select user_name, sum(points) as total from bet group by user_name order by total desc";
 
-		return namedParameterJdbcTemplate.query(sql, new RowMapper<UsernamePoints>() {
-
-			@Override
-			public UsernamePoints mapRow(ResultSet rs, int rowNum) throws SQLException {
-				UsernamePoints usernamePoints = new UsernamePoints();
-				usernamePoints.setUserName(rs.getString(1));
-				usernamePoints.setTotalPoints(rs.getInt(2));
-				return usernamePoints;
-			}
-		});
-	}
+        return jdbcTemplate.query(sql, (ResultSet rs, int rowNum) -> {
+            UsernamePoints usernamePoints = new UsernamePoints();
+            usernamePoints.setUserName(rs.getString(1));
+            usernamePoints.setTotalPoints(rs.getInt(2));
+            return usernamePoints;
+        });
+    }
 }
