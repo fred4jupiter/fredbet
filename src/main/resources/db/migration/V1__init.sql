2bd31cae1d854e994e803d6df19258ce5820df9d
fred4jupiter <hamsterhase@gmx.de>
2016-02-14 10:31:31 +0100

    integrate flyway

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,3 @@
+-- The migrations are scripts in the form V<VERSION>__<NAME>.sql (with <VERSION> an underscore-separated version, e.g. ‘1’ or ‘2_1’)
+-- dummy sql for first script
+Select sysdate from dual;
