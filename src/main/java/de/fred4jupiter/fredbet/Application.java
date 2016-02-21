245eaa6846fad51756503ecdf046f7a782ddab1e
fred4jupiter <hamsterhase@gmx.de>
2016-02-21 08:05:10 +0100

    using hikariCP

--- src/main/java/de/fred4jupiter/fredbet/Application.java
+++ src/main/java/de/fred4jupiter/fredbet/Application.java
@@ -3,11 +3,18 @@ package de.fred4jupiter.fredbet;
 import java.io.IOException;
 import java.util.Properties;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 /**
  * Main application start.
@@ -21,6 +28,9 @@ import org.springframework.core.io.support.PropertiesLoaderUtils;
 @SpringBootApplication
 public class Application {
 
+	@Autowired
+	private Environment environment;
+	
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(Application.class);
 		app.addInitializers(new AppInitializer());
@@ -32,4 +42,22 @@ public class Application {
 		return PropertiesLoaderUtils.loadProperties(new ClassPathResource("build.properties"));
 	}
 
+	@Bean
+	public HikariConfig hikariConfig() {
+		HikariConfig config = new HikariConfig();
+		config.setPoolName("FredBet");
+		config.setConnectionTestQuery("SELECT 1 FROM DUAL");
+		config.setDriverClassName(environment.getProperty("spring.datasource.driverclassName"));
+		config.setJdbcUrl(environment.getProperty("spring.datasource.url"));
+		config.setUsername(environment.getProperty("spring.datasource.username"));
+		config.setPassword(environment.getProperty("spring.datasource.password"));
+		config.setMaximumPoolSize(10);
+		config.setIdleTimeout(30000);
+		return config;
+	}
+
+	@Bean(destroyMethod = "close")
+	public DataSource dataSource() {
+		return new HikariDataSource(hikariConfig());
+	}
 }
