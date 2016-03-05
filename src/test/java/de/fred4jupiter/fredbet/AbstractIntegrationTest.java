3cb61dc402580bcabec5c5d1337ae099084dbdd6
fred4jupiter <hamsterhase@gmx.de>
2016-03-05 17:22:14 +0100

    removed integration test annotation

--- src/test/java/de/fred4jupiter/fredbet/AbstractIntegrationTest.java
+++ src/test/java/de/fred4jupiter/fredbet/AbstractIntegrationTest.java
@@ -1,7 +1,6 @@
 package de.fred4jupiter.fredbet;
 
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -9,7 +8,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @ActiveProfiles(value = { FredBetProfile.DEV, FredBetProfile.INTEGRATION_TEST })
-@IntegrationTest
 public abstract class AbstractIntegrationTest {
 
 }
