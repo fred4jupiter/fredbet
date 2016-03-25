c2f0f7e6aec6e5fc0576d1ed48857e7bdb5b0bce
fred4jupiter <hamsterhase@gmx.de>
2016-03-25 13:19:26 +0100

    added FailedAuthenticationLogHandler

---
+++ src/main/java/de/fred4jupiter/fredbet/security/FailedAuthenticationLogHandler.java
@@ -0,0 +1,21 @@
+package de.fred4jupiter.fredbet.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FailedAuthenticationLogHandler implements ApplicationListener<AuthenticationFailureBadCredentialsEvent> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FailedAuthenticationLogHandler.class);
+    
+    @Override
+    public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent event) {
+        Object username = event.getAuthentication().getPrincipal();
+        Object credentials = event.getAuthentication().getCredentials();
+        LOG.debug("Failed login using username='{}' and password='{}'", username, credentials);
+    }
+
+}
