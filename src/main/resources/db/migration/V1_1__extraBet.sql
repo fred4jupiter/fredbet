beb00169343664454ca626e5d3b4a23f2112d292
fred4jupiter <hamsterhase@gmx.de>
2016-04-23 15:55:38 +0200

    bugfix db migration

--- src/main/resources/db/migration/V1_1__extraBet.sql
+++ src/main/resources/db/migration/V1_1__extraBet.sql
@@ -1,1 +1,1 @@
-create table EXTRA_BET (extra_bet_id bigint not null auto_increment, user_name varchar(255), FINAL_WINNER varchar(255), SEMI_FINAL_WINNER varchar(255), points integer);
+create table EXTRA_BET (extra_bet_id bigint not null auto_increment, user_name varchar(255), FINAL_WINNER varchar(255), SEMI_FINAL_WINNER varchar(255), points integer, primary key (extra_bet_id));
