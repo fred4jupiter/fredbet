b2c3bf6bb12fccaf71c373349a9c5e735d44002d
fred4jupiter <hamsterhase@gmx.de>
2016-04-23 17:32:17 +0200

    bugfix db script

--- src/main/resources/db/migration/V1_1__extraBet.sql
+++ src/main/resources/db/migration/V1_1__extraBet.sql
@@ -1,2 +1,2 @@
-create table EXTRA_BET (extra_bet_id bigint not null auto_increment, user_name varchar(255), FINAL_WINNER varchar(255), SEMI_FINAL_WINNER varchar(255), points integer, primary key (extra_bet_id));
-create unique index EXTRA_BET_IDX1 on EXTRA_BET (user_name);
+create table extra_bet (extra_bet_id bigint not null auto_increment, user_name varchar(255), final_winner varchar(255), semi_final_winner varchar(255), points integer, primary key (extra_bet_id));
+create unique index extra_bet_idx1 on extra_bet (user_name);
