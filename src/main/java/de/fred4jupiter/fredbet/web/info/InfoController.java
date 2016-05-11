5702a02b316252a5bea3dbb490ba1332c2f1f3bf
fred4jupiter <hamsterhase@gmx.de>
2016-05-11 19:38:43 +0200

    edit prices

--- src/main/java/de/fred4jupiter/fredbet/web/info/InfoController.java
+++ src/main/java/de/fred4jupiter/fredbet/web/info/InfoController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import de.fred4jupiter.fredbet.domain.Info;
 import de.fred4jupiter.fredbet.service.InfoService;
+import de.fred4jupiter.fredbet.web.MessageUtil;
 
 @Controller
 @RequestMapping("/info")
@@ -22,6 +24,11 @@ public class InfoController {
 	private static final String INFO_CONTEXT_RULES = "rules";
 
 	private static final Logger LOG = LoggerFactory.getLogger(InfoController.class);
+
+	private static final String INFO_CONTEXT_PRICES = "prices";
+
+	@Autowired
+	private MessageUtil messageUtil;
 
 	@Autowired
 	private InfoService infoService;
@@ -41,7 +48,15 @@ public class InfoController {
 
 	@RequestMapping("/prices")
 	public ModelAndView showPrices() {
-		return new ModelAndView("info/prices");
+		Locale locale = LocaleContextHolder.getLocale();
+		Info info = infoService.findBy(INFO_CONTEXT_PRICES, locale);
+		if (info == null) {
+			info = infoService.saveInfoContent(INFO_CONTEXT_PRICES, "", locale);
+		}
+
+		ModelAndView modelAndView = new ModelAndView("info/prices");
+		modelAndView.addObject("textContent", info.getContent());
+		return modelAndView;
 	}
 
 	@RequestMapping("/editInfo")
@@ -61,7 +76,7 @@ public class InfoController {
 	}
 
 	@RequestMapping(value = "/editInfo", method = RequestMethod.POST)
-	public ModelAndView saveEditedInfo(InfoCommand infoCommand) {
+	public ModelAndView saveEditedInfo(InfoCommand infoCommand, ModelMap modelMap) {
 		LOG.debug("textContent: {}", infoCommand.getTextContent());
 
 		Locale locale = LocaleContextHolder.getLocale();
@@ -69,6 +84,7 @@ public class InfoController {
 
 		ModelAndView modelAndView = new ModelAndView("/info/edit_info");
 		modelAndView.addObject("infoCommand", infoCommand);
+		messageUtil.addInfoMsg(modelMap, "info.msg.edit.saved");
 
 		return modelAndView;
 	}
