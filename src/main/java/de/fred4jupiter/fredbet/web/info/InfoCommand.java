83777334bc553f04d474963627f86566be86159a
fred4jupiter <hamsterhase@gmx.de>
2016-05-11 18:27:41 +0200

    integrated CKEditor

---
+++ src/main/java/de/fred4jupiter/fredbet/web/info/InfoCommand.java
@@ -0,0 +1,14 @@
+package de.fred4jupiter.fredbet.web.info;
+
+public class InfoCommand {
+
+	private String textContent;
+
+	public String getTextContent() {
+		return textContent;
+	}
+
+	public void setTextContent(String textContent) {
+		this.textContent = textContent;
+	}
+}
--- src/main/java/de/fred4jupiter/fredbet/web/info/InfoController.java
+++ src/main/java/de/fred4jupiter/fredbet/web/info/InfoController.java
@@ -1,20 +1,45 @@
 package de.fred4jupiter.fredbet.web.info;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/info")
 public class InfoController {
 
+	private static final Logger LOG = LoggerFactory.getLogger(InfoController.class);
+
 	@RequestMapping("/rules")
 	public ModelAndView showRules() {
 		return new ModelAndView("info/rules");
 	}
-	
+
 	@RequestMapping("/prices")
 	public ModelAndView showPrices() {
 		return new ModelAndView("info/prices");
 	}
+
+	@RequestMapping("/editInfo")
+	public ModelAndView editInfo() {
+		ModelAndView modelAndView = new ModelAndView("/info/edit_info");
+		InfoCommand infoCommand = new InfoCommand();
+		infoCommand.setTextContent("<p>Hello</p>");
+		modelAndView.addObject("infoCommand", infoCommand);
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/editInfo", method = RequestMethod.POST)
+	public ModelAndView saveEditedInfo(InfoCommand infoCommand) {
+		LOG.debug("textContent: {}", infoCommand.getTextContent());
+
+		ModelAndView modelAndView = new ModelAndView("/info/edit_info");
+		modelAndView.addObject("infoCommand", infoCommand);
+		
+		return modelAndView;
+	}
+
 }
