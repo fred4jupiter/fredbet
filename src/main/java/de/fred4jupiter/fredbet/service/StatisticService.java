c859a1b3901d78d40ad9ef30ee93c7b5d0f3d323
fred4jupiter <hamsterhase@gmx.de>
2016-06-18 13:29:55 +0200

    refactoring statisticsService; changed color for highlight statistic

--- src/main/java/de/fred4jupiter/fredbet/service/StatisticService.java
+++ src/main/java/de/fred4jupiter/fredbet/service/StatisticService.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
@@ -19,16 +17,13 @@ import de.fred4jupiter.fredbet.repository.StatisticRepository;
 @Service
 public class StatisticService {
 
-	@Autowired
-	private StatisticRepository statisticRepository;
+	private final StatisticRepository statisticRepository;
+
+	private final Country favoriteCountry;
 
 	@Autowired
-	private Environment environment;
-
-	private Country favoriteCountry;
-
-	@PostConstruct
-	private void init() {
+	public StatisticService(Environment environment, StatisticRepository statisticRepository) {
+		this.statisticRepository = statisticRepository;
 		String favoriteCountryString = environment.getProperty("favorite.country", Country.GERMANY.name());
 		this.favoriteCountry = Country.fromName(favoriteCountryString);
 		Assert.notNull(this.favoriteCountry);
@@ -37,9 +32,10 @@ public class StatisticService {
 	public List<Statistic> createStatistic() {
 		List<Statistic> statisticList = statisticRepository.createStatistic();
 
-		final Map<String, Integer> favoriteCountryPointsPerUserMap = statisticRepository.sumPointsPerUserForFavoriteCountry(favoriteCountry);
+		final Map<String, Integer> favoriteCountryPointsPerUserMap = statisticRepository
+				.sumPointsPerUserForFavoriteCountry(favoriteCountry);
 		Optional<Integer> maxOptional = favoriteCountryPointsPerUserMap.values().stream().max(Comparator.comparing(i -> i));
-		
+
 		for (Statistic statistic : statisticList) {
 			statistic.setFavoriteCountry(this.favoriteCountry);
 			final Integer favoriteCountryPoints = favoriteCountryPointsPerUserMap.get(statistic.getUsername());
