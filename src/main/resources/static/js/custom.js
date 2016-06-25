28f71045c1d7e867df0db77722f78c8d66016074
fred4jupiter <hamsterhase@gmx.de>
2016-06-25 14:50:00 +0200

    update layout for voting; added buttons for up / down

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -16,3 +16,31 @@ function clearTeamNameFields() {
 	$('#nameTeamOne').val("");
 	$('#nameTeamTwo').val("");
 }
+
+function oneVoteUp() {
+	var val = $('#goalsTeamOne').val();
+	$('#goalsTeamOne').val((val*1)+1); 
+	checkPenalty();
+}
+
+function oneVoteDown() {
+	var val = $('#goalsTeamOne').val();
+	if (val > 0) {
+		$('#goalsTeamOne').val((val*1)-1);
+		checkPenalty();
+	}
+}
+
+function twoVoteUp() {
+	var val = $('#goalsTeamTwo').val();
+	$('#goalsTeamTwo').val((val*1)+1);
+	checkPenalty();
+}
+
+function twoVoteDown() {
+	var val = $('#goalsTeamTwo').val();
+	if (val > 0) {
+		$('#goalsTeamTwo').val((val*1)-1); 
+		checkPenalty();
+	}
+}
