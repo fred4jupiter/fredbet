5720ff645aeca0b76f33eaee57e785d843f7c532
fred4jupiter <hamsterhase@gmx.de>
2016-07-31 19:58:38 +0200

    update to Spring Boot 1.4.0.RELEASE

--- src/test/java/de/fred4jupiter/fredbet/AbstractIntegrationTest.java
+++ src/test/java/de/fred4jupiter/fredbet/AbstractIntegrationTest.java
@@ -1,12 +1,12 @@
 package de.fred4jupiter.fredbet;
 
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@SpringBootTest(classes = Application.class)
 @ActiveProfiles(value = { FredBetProfile.DEV, FredBetProfile.INTEGRATION_TEST })
 public abstract class AbstractIntegrationTest {
 
