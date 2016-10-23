e38ec93a2a9cfdbebe06df3a69081678cf8baf2e
fred4jupiter <hamsterhase@gmx.de>
2016-10-23 19:54:38 +0200

    update image upload

--- src/main/java/de/fred4jupiter/fredbet/service/ImageUploadService.java
+++ src/main/java/de/fred4jupiter/fredbet/service/ImageUploadService.java
@@ -1,5 +1,9 @@
 package de.fred4jupiter.fredbet.service;
 
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,5 +21,19 @@ public class ImageUploadService {
 	public void saveImageInDatabase(String fileName, byte[] binary) {
 		FileStorage fileStorage = new FileStorage(fileName, binary);
 		fileStorageRepository.save(fileStorage);
+	}
+
+	public List<String> fetchAllImagesAsBase64() {
+		List<FileStorage> allSavedImages = fileStorageRepository.findAll();
+
+		List<String> allImagesAsBase64 = new ArrayList<>();
+
+		for (FileStorage fileStorage : allSavedImages) {
+			byte[] imageBinary = fileStorage.getImageBinary();
+			String encodeToString = Base64.getEncoder().encodeToString(imageBinary);
+			allImagesAsBase64.add(encodeToString);
+		}
+
+		return allImagesAsBase64;
 	}
 }
--- src/main/java/de/fred4jupiter/fredbet/web/image/ImageUploadController.java
+++ src/main/java/de/fred4jupiter/fredbet/web/image/ImageUploadController.java
@@ -1,11 +1,13 @@
 package de.fred4jupiter.fredbet.web.image;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,6 +29,11 @@ public class ImageUploadController {
 
 	@Autowired
 	private MessageUtil messageUtil;
+	
+	@ModelAttribute("availableImages")
+	public List<String> availableImages() {
+		return imageUploadService.fetchAllImagesAsBase64();
+	}
 
 	@RequestMapping(value = "/show", method = RequestMethod.GET)
 	public ModelAndView showUploadPage() {
