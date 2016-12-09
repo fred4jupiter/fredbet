c639073d4d0bd549d788a8baef78dd3d14e6dc50
fred4jupiter <hamsterhase@gmx.de>
2016-12-09 19:32:30 +0100

    update release script

--- release_only.sh
+++ release_only.sh
@@ -21,17 +21,11 @@ fi
 
 if [ -z "$3" ]
   then
-    echo "The DOCKER_EMAIL has to be given as third parameter."
-    exit 1
-fi
-
-if [ -z "$4" ]
-  then
     echo "The DOCKER_USERNAME has to be given fourth parameter."
     exit 1
 fi
 
-if [ -z "$5" ]
+if [ -z "$4" ]
   then
     echo "The DOCKER_PASSWORD has to be given fourth parameter."
     exit 1
@@ -39,9 +33,8 @@ fi
 
 GITHUB_USERNAME=$1
 GITHUB_PASSWORD=$2
-DOCKER_EMAIL=$3
-DOCKER_USERNAME=$4
-DOCKER_PASSWORD=$5
+DOCKER_USERNAME=$3
+DOCKER_PASSWORD=$4
 
 git config user.name "fred4jupiter"
 git config user.email "hamsterhase@gmx.de"
@@ -50,6 +43,7 @@ mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorV
 mvn build-helper:parse-version scm:tag -Dbasedir=. -Dtag=release_\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion} -Dusername=$GITHUB_USERNAME -Dpassword=$GITHUB_PASSWORD
 mvn clean package -DskipTests
 PROJECT_REL_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+docker build -t fred4jupiter/fredbet .
 
 mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}-SNAPSHOT versions:commit
 mvn -Dmessage="next dev version $NEXT_DEV_VERSION" -Dbasedir=. scm:checkin -DpushChanges=true
@@ -61,10 +55,11 @@ echo "next development version is: $NEXT_DEV_VERSION"
 
 
 # docker image tagging and publishing in Docker Hub
-docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker tag -f fred4jupiter/fredbet hamsterhase/fredbet:$PROJECT_REL_VERSION
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker build 
+docker tag fred4jupiter/fredbet hamsterhase/fredbet:$PROJECT_REL_VERSION
 docker push hamsterhase/fredbet:$PROJECT_REL_VERSION
-docker tag -f fred4jupiter/fredbet hamsterhase/fredbet:latest
+docker tag fred4jupiter/fredbet hamsterhase/fredbet:latest
 docker push hamsterhase/fredbet:latest
 
 docker logout
