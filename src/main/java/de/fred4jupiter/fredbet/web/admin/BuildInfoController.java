caf45b7a341cb5eb9af6ff621fa4420b72d97285
fred4jupiter <hamsterhase@gmx.de>
2017-01-09 19:38:13 +0100

    added spring boot metrics uptime in build info page

--- src/main/java/de/fred4jupiter/fredbet/web/admin/BuildInfoController.java
+++ src/main/java/de/fred4jupiter/fredbet/web/admin/BuildInfoController.java
@@ -8,13 +8,16 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.MetricsEndpoint;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -28,14 +31,17 @@ public class BuildInfoController {
 	@Autowired
 	private Properties buildProperties;
 
+	@Autowired
+	private MetricsEndpoint metricsEndpoint;
+
 	@PostConstruct
 	private void postProcessBuildProperties() {
 		String buildTimestamp = buildProperties.getProperty("build.timestamp");
 		buildTimestamp = buildTimestamp + " +0000";
 
 		ZonedDateTime parsed = ZonedDateTime.parse(buildTimestamp, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm Z"));
-		String formattedDateTime = parsed
-				.format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm zZ", Locale.getDefault()).withZone(ZoneId.of("Europe/Berlin")));
+		String formattedDateTime = parsed.format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm zZ", Locale.getDefault())
+				.withZone(ZoneId.of("Europe/Berlin")));
 		buildProperties.put("build.timestamp", formattedDateTime);
 	}
 
@@ -49,6 +55,19 @@ public class BuildInfoController {
 	private void addDynamicInfoProperties() {
 		addCurrentDateTime();
 		addHostName();
+		addMetrics();
+	}
+
+	private void addMetrics() {
+		Map<String, Object> metricsMap = metricsEndpoint.invoke();
+
+		buildProperties.put("system.uptime", formatUptime((Long) metricsMap.get("uptime")));
+		buildProperties.put("application.context.uptime", formatUptime((Long) metricsMap.get("instance.uptime")));
+	}
+
+	private String formatUptime(Long millis) {
+		return String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millis),
+				TimeUnit.MILLISECONDS.toMinutes(millis), TimeUnit.MILLISECONDS.toSeconds(millis));
 	}
 
 	private void addHostName() {
@@ -66,7 +85,8 @@ public class BuildInfoController {
 		DateTimeFormatter timeZoneFormatter = DateTimeFormatter.ofPattern("VV x");
 
 		final String key = "currentDateTime";
-		final String value = formatter.format(LocalDateTime.now()) + ", " + timeZoneFormatter.format(ZonedDateTime.now());
+		final String value = formatter.format(LocalDateTime.now()) + ", "
+				+ timeZoneFormatter.format(ZonedDateTime.now());
 
 		buildProperties.put(key, value);
 	}
