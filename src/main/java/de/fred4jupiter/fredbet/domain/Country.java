fdca4ce8ce2bd9afb7e7a4a64f6f37d2c8626bd6
fred4jupiter <hamsterhase@gmx.de>
2017-02-26 14:57:18 +0100

    added autralia

--- src/main/java/de/fred4jupiter/fredbet/domain/Country.java
+++ src/main/java/de/fred4jupiter/fredbet/domain/Country.java
@@ -2,79 +2,39 @@ package de.fred4jupiter.fredbet.domain;
 
 public enum Country {
 
-	NONE("none"),
-	
-	ALBANIA("al"),
+    NONE("none"), // non selected country placeholder
 
-	BELGIUM("be"),
+    ALBANIA("al"), BELGIUM("be"), GERMANY("de"), ENGLAND("en"), FRANCE("fr"), IRELAND("ie"), ICELAND("is"),
 
-	GERMANY("de"),
+    ITALY("it"), CROATIA("hr"), NORTH_IRELAND("north_ireland"), AUSTRIA("at"), POLAND("pl"), PORTUGAL("pt"), ROMANIA("ro"),
 
-	ENGLAND("en"),
+    RUSSIA("ru"), SWEDEN("se"), SWITZERLAND("ch"), SLOVAKIA("sk"), SPAIN("es"), CZECH_REPUBLIC("cz"), TURKEY("tr"),
 
-	FRANCE("fr"),
+    UKRAINE("ua"), HUNGARY("hu"), WALES("wales"), AUSTRALIA("au");
 
-	IRELAND("ie"),
+    private static final String ICON_BASE_PATH = "/images/flags/";
 
-	ICELAND("is"),
+    private String isoCode;
 
-	ITALY("it"),
+    private Country(String isoCode) {
+        this.isoCode = isoCode;
+    }
 
-	CROATIA("hr"),
+    public String getIsoCode() {
+        return isoCode;
+    }
 
-	NORTH_IRELAND("north_ireland"),
+    public static Country fromName(String name) {
+        for (Country country : values()) {
+            if (country.name().equals(name)) {
+                return country;
+            }
+        }
 
-	AUSTRIA("at"),
+        return null;
+    }
 
-	POLAND("pl"),
-
-	PORTUGAL("pt"),
-
-	ROMANIA("ro"),
-
-	RUSSIA("ru"),
-
-	SWEDEN("se"),
-
-	SWITZERLAND("ch"),
-
-	SLOVAKIA("sk"),
-
-	SPAIN("es"),
-
-	CZECH_REPUBLIC("cz"),
-
-	TURKEY("tr"),
-
-	UKRAINE("ua"),
-
-	HUNGARY("hu"),
-
-	WALES("wales");
-
-	private static final String ICON_BASE_PATH = "/images/flags/";  
-	
-	private String isoCode;
-
-	private Country(String isoCode) {
-		this.isoCode = isoCode;
-	}
-
-	public String getIsoCode() {
-		return isoCode;
-	}
-	
-	public static Country fromName(String name) {
-		for (Country country : values()) {
-			if (country.name().equals(name)) {
-				return country;
-			}
-		}
-		
-		return null;
-	}
-
-	public String getIconPath() {
-		return ICON_BASE_PATH + isoCode + ".png";
-	}
+    public String getIconPath() {
+        return ICON_BASE_PATH + isoCode + ".png";
+    }
 }
