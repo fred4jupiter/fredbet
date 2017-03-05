e818ccefb58e122330815c767c01f48ff6775b3b
fred4jupiter <hamsterhase@gmx.de>
2017-03-05 20:01:22 +0100

    update AWS config

--- src/main/java/de/fred4jupiter/fredbet/AwsConfiguration.java
+++ src/main/java/de/fred4jupiter/fredbet/AwsConfiguration.java
@@ -1,13 +1,24 @@
 package de.fred4jupiter.fredbet;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+import com.amazonaws.services.s3.AmazonS3;
+
+import de.fred4jupiter.fredbet.service.image.AmazonS3ClientWrapper;
 
 @Configuration
 @ImportResource("classpath:/aws-config.xml")
 @ConditionalOnProperty(prefix = "fredbet", name = "image-location", havingValue = "aws-s3")
 public class AwsConfiguration {
 
-    
+	@Bean
+	public AmazonS3ClientWrapper amazonS3ClientWrapper(AmazonS3 amazonS3, FredbetProperties fredbetProperties, ResourceLoader resourceLoader,
+			ResourcePatternResolver resourcePatternResolver) {
+		return new AmazonS3ClientWrapper(amazonS3, fredbetProperties.getBucket(), resourceLoader, resourcePatternResolver);
+	}
 }
