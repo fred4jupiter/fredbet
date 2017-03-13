ff962136837eb8219fc967413ba71584d3e683e2
fred4jupiter <hamsterhase@gmx.de>
2017-03-13 19:34:25 +0100

    bugfix test

--- src/test/java/de/fred4jupiter/fredbet/service/RankingServiceIT.java
+++ src/test/java/de/fred4jupiter/fredbet/service/RankingServiceIT.java
@@ -11,15 +11,20 @@ import static org.junit.Assert.assertThat;
 import java.util.List;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import de.fred4jupiter.fredbet.AbstractTransactionalIntegrationTest;
 import de.fred4jupiter.fredbet.data.DataBasePopulator;
+import de.fred4jupiter.fredbet.domain.AppUser;
 import de.fred4jupiter.fredbet.domain.AppUserBuilder;
 import de.fred4jupiter.fredbet.props.FredbetConstants;
 import de.fred4jupiter.fredbet.repository.UsernamePoints;
 
 public class RankingServiceIT extends AbstractTransactionalIntegrationTest {
+
+	private static final Logger LOG = LoggerFactory.getLogger(RankingServiceIT.class);
 
 	@Autowired
 	private DataBasePopulator dataBasePopulator;
@@ -32,12 +37,13 @@ public class RankingServiceIT extends AbstractTransactionalIntegrationTest {
 
 	@Test
 	public void getRankingForUsersButWithoutTechnicalUser() {
-		userService.insertAppUser(AppUserBuilder.create().withDemoData().build());
-		userService.insertAppUser(AppUserBuilder.create().withDemoData().build());
-		userService.insertAppUser(AppUserBuilder.create().withDemoData().build());
-		userService.insertAppUser(AppUserBuilder.create().withDemoData().build());
-		userService.insertAppUser(
-				AppUserBuilder.create().withDemoData().withUsernameAndPassword(FredbetConstants.TECHNICAL_USERNAME, "test").build());
+		saveIfNotPresent(AppUserBuilder.create().withDemoData().build());
+		saveIfNotPresent(AppUserBuilder.create().withDemoData().build());
+		saveIfNotPresent(AppUserBuilder.create().withDemoData().build());
+		saveIfNotPresent(AppUserBuilder.create().withDemoData().build());
+
+		final String username = FredbetConstants.TECHNICAL_USERNAME;
+		saveIfNotPresent(AppUserBuilder.create().withDemoData().withUsernameAndPassword(username, "test").build());
 
 		dataBasePopulator.createEM2016Matches();
 		dataBasePopulator.createDemoBetsForAllUsers();
@@ -46,6 +52,14 @@ public class RankingServiceIT extends AbstractTransactionalIntegrationTest {
 		List<UsernamePoints> rankings = rankingService.calculateCurrentRanking();
 		assertNotNull(rankings);
 		assertFalse(rankings.isEmpty());
-		assertThat(rankings, not(hasItem(hasProperty("userName", equalTo(FredbetConstants.TECHNICAL_USERNAME)))));
+		assertThat(rankings, not(hasItem(hasProperty("userName", equalTo(username)))));
+	}
+
+	private void saveIfNotPresent(AppUser appUser) {
+		try {
+			userService.insertAppUser(appUser);
+		} catch (UserAlreadyExistsException e) {
+			LOG.debug(e.getMessage());
+		}
 	}
 }
