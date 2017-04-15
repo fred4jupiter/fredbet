81d4a497423a7f569cbc0f332ef729deef57ea14
fred4jupiter <hamsterhase@gmx.de>
2017-04-15 14:38:15 +0200

    showing user image profile

--- src/main/java/de/fred4jupiter/fredbet/security/SecurityService.java
+++ src/main/java/de/fred4jupiter/fredbet/security/SecurityService.java
@@ -5,9 +5,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import de.fred4jupiter.fredbet.domain.AppUser;
+import de.fred4jupiter.fredbet.domain.ImageMetaData;
 import de.fred4jupiter.fredbet.props.FredbetProperties;
+import de.fred4jupiter.fredbet.service.UserService;
 
 /**
  * Provides security informations of the current user.
@@ -20,6 +23,9 @@ public class SecurityService {
 
 	@Autowired
 	private FredbetProperties fredbetProperties;
+
+	@Autowired
+	private UserService userService;
 
 	public boolean isUserLoggedIn() {
 		try {
@@ -49,18 +55,27 @@ public class SecurityService {
 
 	public boolean isRoleSelectionDisabledForUser(String username) {
 		final AppUser currentUser = getCurrentUser();
-		return currentUser.getUsername().equals(username)
-				|| !(currentUser.hasPermission(FredBetPermission.PERM_CHANGE_USER_ROLE));
+		return currentUser.getUsername().equals(username) || !(currentUser.hasPermission(FredBetPermission.PERM_CHANGE_USER_ROLE));
 	}
 
 	public AppUser getCurrentUser() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		if (authentication == null || !authentication.isAuthenticated()
-				|| !(authentication.getPrincipal() instanceof AppUser)) {
+		if (authentication == null || !authentication.isAuthenticated() || !(authentication.getPrincipal() instanceof AppUser)) {
 			throw new UsernameNotFoundException("User is not logged in!");
 		}
 
 		return (AppUser) authentication.getPrincipal();
 	}
 
+	@Transactional(readOnly = true)
+	public Long getCurrentUserProfileImageId() {
+		AppUser currentUser = getCurrentUser();
+		if (currentUser == null) {
+			return null;
+		}
+
+		AppUser appUser = userService.findByAppUserId(currentUser.getId());
+		ImageMetaData imageMetaData = appUser.getUserProfileImageMetaData();
+		return imageMetaData != null ? imageMetaData.getId() : null;
+	}
 }
