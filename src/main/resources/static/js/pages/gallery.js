65cc4d0d89ee7c471398a448dd958c8ce46b83f1
fred4jupiter <hamsterhase@gmx.de>
2017-04-22 14:03:11 +0200

    refactored js include gallery

---
+++ src/main/resources/static/js/pages/gallery.js
@@ -0,0 +1,11 @@
+document.getElementById('links').onclick = function(event) {
+	event = event || window.event;
+	var target = event.target || event.srcElement, link = target.src ? target.parentNode
+			: target, options = {
+		index : link,
+		event : event,
+		fullScreen : true
+	}, 
+	links = this.getElementsByTagName('a');
+	blueimp.Gallery(links, options);
+};
