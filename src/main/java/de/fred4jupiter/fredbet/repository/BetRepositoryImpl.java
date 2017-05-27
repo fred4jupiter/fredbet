e1477d9c973fbea266617339c3e9b0950348e429
fred4jupiter <hamsterhase@gmx.de>
2017-05-27 10:30:07 +0200

    reformat

--- src/main/java/de/fred4jupiter/fredbet/repository/BetRepositoryImpl.java
+++ src/main/java/de/fred4jupiter/fredbet/repository/BetRepositoryImpl.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 
@@ -12,22 +14,28 @@ import de.fred4jupiter.fredbet.props.FredbetConstants;
 
 class BetRepositoryImpl implements BetRepositoryCustom {
 
+	private static final Logger LOG = LoggerFactory.getLogger(BetRepositoryImpl.class);
+
 	@Autowired
 	private NamedParameterJdbcOperations namedParameterJdbcOperations;
 
 	public List<UsernamePoints> calculateRanging() {
 		StringBuilder builder = new StringBuilder();
 		builder.append("Select user_name, sum(total) as sum_all from (");
-		builder.append("(Select user_name, sum(points) as total from bet where user_name not like :username group by user_name order by total desc) ");
+		builder.append("(Select user_name, sum(points) as total ");
+		builder.append("from bet where user_name not like :username group by user_name order by total desc) ");
 		builder.append("union all ");
-		builder.append("(Select user_name, (points_one + points_two + points_three) as total from extra_bet where user_name not like :username group by user_name order by total desc) ");
+		builder.append("(Select user_name, (points_one + points_two + points_three) as total ");
+		builder.append("from extra_bet where user_name not like :username group by user_name order by total desc) ");
 		builder.append(") as complete ");
 		builder.append("group by user_name order by sum_all desc");
-		
+
 		Map<String, Object> params = new HashMap<>();
 		params.put("username", FredbetConstants.TECHNICAL_USERNAME);
-		
-		return namedParameterJdbcOperations.query(builder.toString(), params, (ResultSet rs, int rowNum) -> {
+
+		final String sql = builder.toString();
+		LOG.debug("sql={}", sql);
+		return namedParameterJdbcOperations.query(sql, params, (ResultSet rs, int rowNum) -> {
 			UsernamePoints usernamePoints = new UsernamePoints();
 			usernamePoints.setUserName(rs.getString(1));
 			usernamePoints.setTotalPoints(rs.getInt(2));
