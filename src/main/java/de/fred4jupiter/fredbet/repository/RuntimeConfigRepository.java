95eb4158630ea3ea365012c919e16fcef4b76a3b
fred4jupiter <hamsterhase@gmx.de>
2018-01-27 14:50:59 +0100

    refactoring config

--- src/main/java/de/fred4jupiter/fredbet/repository/RuntimeConfigRepository.java
+++ src/main/java/de/fred4jupiter/fredbet/repository/RuntimeConfigRepository.java
@@ -3,95 +3,61 @@ package de.fred4jupiter.fredbet.repository;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Repository;
 
 import com.google.gson.Gson;
 
-import de.fred4jupiter.fredbet.domain.RuntimeConfig;
 import de.fred4jupiter.fredbet.domain.RuntimeConfigDb;
-import de.fred4jupiter.fredbet.props.FredBetProfile;
-import de.fred4jupiter.fredbet.props.FredbetConstants;
 
 @Repository
 public class RuntimeConfigRepository {
 
 	private static final Logger LOG = LoggerFactory.getLogger(RuntimeConfigRepository.class);
 
-	private static final Long DEFAULT_CONFIG_ID = Long.valueOf(1);
-
 	@Autowired
 	private RuntimeConfigDbRepository runtimeConfigDbRepository;
 
-	@Autowired
-	private Environment environment;
-
-	public RuntimeConfig loadRuntimeConfig() {
-		RuntimeConfigDb runtimeConfigDb = runtimeConfigDbRepository.findOne(DEFAULT_CONFIG_ID);
+	public <T> T loadRuntimeConfig(Long id, Class<T> targetType) {
+		RuntimeConfigDb runtimeConfigDb = runtimeConfigDbRepository.findOne(id);
 		if (runtimeConfigDb == null) {
-			runtimeConfigDb = new RuntimeConfigDb(DEFAULT_CONFIG_ID);
+			runtimeConfigDb = new RuntimeConfigDb(id);
 			runtimeConfigDbRepository.save(runtimeConfigDb);
 		}
 
-		return toRuntimeConfig(runtimeConfigDb);
+		return toRuntimeConfig(runtimeConfigDb, targetType);
 	}
 
-	private RuntimeConfig toRuntimeConfig(RuntimeConfigDb runtimeConfigDb) {
+	private <T> T toRuntimeConfig(RuntimeConfigDb runtimeConfigDb, Class<T> targetType) {
 		String jsonConfig = runtimeConfigDb.getJsonConfig();
 
 		if (StringUtils.isNotBlank(jsonConfig)) {
 			Gson gson = new Gson();
-			return gson.fromJson(jsonConfig, RuntimeConfig.class);
-		} else {
-			return createDefaultRuntimeConfig();
-		}
-	}
-
-	private RuntimeConfig createDefaultRuntimeConfig() {
-		RuntimeConfig runtimeConfig = new RuntimeConfig();
-		runtimeConfig.setFavouriteCountry(FredbetConstants.DEFAULT_FAVOURITE_COUNTRY);
-		runtimeConfig.setPasswordForReset(FredbetConstants.DEFAULT_REST_PASSWORT);
-
-		if (environment.acceptsProfiles(FredBetProfile.DEV)) {
-			runtimeConfig.setShowDemoDataNavigationEntry(true);
-			runtimeConfig.setEnableChangingUsername(true);
-			runtimeConfig.setCreateDemoData(true);
-			runtimeConfig.setEnabledParentChildRanking(true);
-
-			saveRuntimeConfig(runtimeConfig);
+			return gson.fromJson(jsonConfig, targetType);
 		}
 
-		return runtimeConfig;
+		return null;
 	}
 
-	public void saveRuntimeConfig(RuntimeConfig runtimeConfig) {
-		RuntimeConfig runtimeConfigFromDb = toRuntimeConfig(loadRuntimeConfigDb());
-		updatePropertiesFromTo(runtimeConfig, runtimeConfigFromDb);
-
-		RuntimeConfigDb runtimeConfigDb = toRuntimeConfigDb(runtimeConfigFromDb);
+	public <T> void saveRuntimeConfig(Long id, T runtimeConfig) {
+		RuntimeConfigDb runtimeConfigDb = toRuntimeConfigDb(id, runtimeConfig);
 
 		runtimeConfigDbRepository.save(runtimeConfigDb);
 		LOG.info("saved runtime configuration.");
 	}
 
-	private void updatePropertiesFromTo(RuntimeConfig from, RuntimeConfig to) {
-		BeanUtils.copyProperties(from, to);
-	}
-
-	private RuntimeConfigDb toRuntimeConfigDb(RuntimeConfig runtimeConfig) {
+	private <T> RuntimeConfigDb toRuntimeConfigDb(Long id, T runtimeConfig) {
 		Gson gson = new Gson();
 		String json = gson.toJson(runtimeConfig);
-		RuntimeConfigDb runtimeConfigDb = loadRuntimeConfigDb();
+		RuntimeConfigDb runtimeConfigDb = loadRuntimeConfigDb(id);
 		runtimeConfigDb.setJsonConfig(json);
 		return runtimeConfigDb;
 	}
 
-	private RuntimeConfigDb loadRuntimeConfigDb() {
-		RuntimeConfigDb runtimeConfigDb = runtimeConfigDbRepository.findOne(DEFAULT_CONFIG_ID);
+	private RuntimeConfigDb loadRuntimeConfigDb(Long id) {
+		RuntimeConfigDb runtimeConfigDb = runtimeConfigDbRepository.findOne(id);
 		if (runtimeConfigDb == null) {
-			runtimeConfigDb = new RuntimeConfigDb(DEFAULT_CONFIG_ID);
+			runtimeConfigDb = new RuntimeConfigDb(id);
 			runtimeConfigDbRepository.save(runtimeConfigDb);
 		}
 
--- src/main/java/de/fred4jupiter/fredbet/service/config/RuntimeConfigurationService.java
+++ src/main/java/de/fred4jupiter/fredbet/service/config/RuntimeConfigurationService.java
@@ -5,11 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import de.fred4jupiter.fredbet.domain.RuntimeConfig;
 import de.fred4jupiter.fredbet.props.CacheNames;
+import de.fred4jupiter.fredbet.props.FredBetProfile;
+import de.fred4jupiter.fredbet.props.FredbetConstants;
 import de.fred4jupiter.fredbet.repository.RuntimeConfigRepository;
 
 /**
@@ -24,18 +27,45 @@ public class RuntimeConfigurationService {
 
 	private static final Logger LOG = LoggerFactory.getLogger(RuntimeConfigurationService.class);
 
+	private static final Long DEFAULT_CONFIG_ID = Long.valueOf(1);
+
 	@Autowired
 	private RuntimeConfigRepository runtimeConfigRepository;
+
+	@Autowired
+	private Environment environment;
 
 	@Cacheable(CacheNames.RUNTIME_CONFIG)
 	public RuntimeConfig loadRuntimeConfig() {
 		LOG.debug("Loading runtime configuration from DB...");
-		return runtimeConfigRepository.loadRuntimeConfig();
+		RuntimeConfig runtimeConfig = runtimeConfigRepository.loadRuntimeConfig(DEFAULT_CONFIG_ID, RuntimeConfig.class);
+		if (runtimeConfig == null) {
+			runtimeConfig = createDefaultRuntimeConfig();
+		}
+
+		return runtimeConfig;
+	}
+
+	private RuntimeConfig createDefaultRuntimeConfig() {
+		RuntimeConfig runtimeConfig = new RuntimeConfig();
+		runtimeConfig.setFavouriteCountry(FredbetConstants.DEFAULT_FAVOURITE_COUNTRY);
+		runtimeConfig.setPasswordForReset(FredbetConstants.DEFAULT_REST_PASSWORT);
+
+		if (environment.acceptsProfiles(FredBetProfile.DEV)) {
+			runtimeConfig.setShowDemoDataNavigationEntry(true);
+			runtimeConfig.setEnableChangingUsername(true);
+			runtimeConfig.setCreateDemoData(true);
+			runtimeConfig.setEnabledParentChildRanking(true);
+
+			saveRuntimeConfig(runtimeConfig);
+		}
+
+		return runtimeConfig;
 	}
 
 	@CacheEvict(cacheNames = CacheNames.RUNTIME_CONFIG, allEntries = true)
 	public void saveRuntimeConfig(RuntimeConfig runtimeConfig) {
-		runtimeConfigRepository.saveRuntimeConfig(runtimeConfig);
+		runtimeConfigRepository.saveRuntimeConfig(DEFAULT_CONFIG_ID, runtimeConfig);
 	}
 
 }
