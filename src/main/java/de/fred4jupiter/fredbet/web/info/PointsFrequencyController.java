fd949234430952fde6c037991e3db192af5bd1d4
fred4jupiter <hamsterhase@gmx.de>
2018-03-11 19:38:05 +0100

    small ajustments points frequency

--- src/main/java/de/fred4jupiter/fredbet/web/info/PointsFrequencyController.java
+++ src/main/java/de/fred4jupiter/fredbet/web/info/PointsFrequencyController.java
@@ -3,12 +3,14 @@ package de.fred4jupiter.fredbet.web.info;
 import org.apache.commons.collections4.MultiValuedMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import de.fred4jupiter.fredbet.repository.PointCountResult;
 import de.fred4jupiter.fredbet.service.excel.ReportService;
+import de.fred4jupiter.fredbet.web.WebMessageUtil;
 
 @Controller
 @RequestMapping("/info/pointsfrequency")
@@ -17,11 +19,20 @@ public class PointsFrequencyController {
 	@Autowired
 	private ReportService reportService;
 
+	@Autowired
+	private WebMessageUtil webMessageUtil;
+
 	@GetMapping
-	public ModelAndView show() {
+	public ModelAndView show(ModelMap modelMap) {
 		MultiValuedMap<Integer, PointCountResult> map = reportService.reportPointsFrequency();
 
-		ModelAndView modelAndView = new ModelAndView("info/points_freq");
+		final ModelAndView modelAndView = new ModelAndView("info/points_freq");
+
+		if (map.isEmpty()) {
+			webMessageUtil.addInfoMsg(modelMap, "pointsfrequency.noData");
+			return modelAndView;
+		}
+
 		modelAndView.addObject("fourPointsMap", map.get(4));
 		modelAndView.addObject("threePointsMap", map.get(3));
 		modelAndView.addObject("twoPointsMap", map.get(2));
