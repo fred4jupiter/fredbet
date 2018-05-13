80187356b39e6eb336be3ce3e5f09e37a97dbe84
fred4jupiter <hamsterhase@gmx.de>
2018-05-13 18:33:49 +0200

    bugfix image cropping

--- src/main/resources/static/js/pages/crop_image.js
+++ src/main/resources/static/js/pages/crop_image.js
@@ -5,18 +5,31 @@ $(document).ready(function() {
 function imageCroppingUpload() {
 	var $uploadCrop;
 
+	var opts = {
+		viewport: {
+			width: 150,
+			height: 150,			
+		},
+		type: 'canvas',
+		size: 'viewport',
+		enableResize: true,
+		enableExif: true,
+		enableOrientation: true,
+		format: 'jpeg'
+	};
+	
 	function readFile(input) {
 			if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            
-            reader.onload = function (e) {				
-            	$uploadCrop.croppie('bind', {
-            		url: e.target.result
-            	}).then(function(){
-            		console.log('jQuery bind complete');
-            	});
-            }
-            reader.readAsDataURL(input.files[0]);
+	        var reader = new FileReader();
+	        
+	        reader.onload = function (e) {				
+	        	$uploadCrop.croppie('bind', {
+	        		url: e.target.result
+	        	}).then(function(){
+	        		console.log('jQuery bind complete');
+	        	});
+	        }
+	        reader.readAsDataURL(input.files[0]);
         }
         else {
 	        console.log("Sorry - you're browser doesn't support the FileReader API");
@@ -25,28 +38,14 @@ function imageCroppingUpload() {
 
 	$('#upload').on('change', function () { readFile(this); });
 	
-	$uploadCrop = $('#croppie-container').croppie({
-		viewport: {
-			width: 150,
-			height: 150,			
-		},
-		enableExif: true,
-		enableOrientation: true,
-		format: 'jpeg'		
-	});
+	$uploadCrop = $('#croppie-container').croppie(opts);
 	
 	$('.crop-image-rotate').on('click', function(ev) {
 		$uploadCrop.croppie('rotate', parseInt($(this).data('deg')));
 	});
 	
 	$('#crop-image').on('click', function (ev) {
-		$uploadCrop.croppie('result', {
-			type: 'canvas',
-			size: 'viewport',
-			enableExif: true,
-			enableOrientation: true,
-			format: 'jpeg'
-		}).then(function (resp) {
+		$uploadCrop.croppie('result', opts).then(function (resp) {
 			$("#resultImage").attr("src", resp);
 			$("#croppedFileBase64").attr("value", resp);
 		});
