88dfe6d0ea53dc82ec0bf946215d71c1e86d4974
fred4jupiter <hamsterhase@gmx.de>
2018-05-31 14:50:15 +0200

    cleanup

--- src/main/java/de/fred4jupiter/fredbet/service/image/storage/AmazonS3ClientWrapper.java
+++ src/main/java/de/fred4jupiter/fredbet/service/image/storage/AmazonS3ClientWrapper.java
@@ -17,7 +17,6 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.ObjectMetadata;
