a8b9f7f608dc432b48bd18f529c05a0babc2e415
Staehler, Michael <michael.staehler@opitz-consulting.com>
2018-07-08 20:06:33 +0200

    added goal difference in ranking

--- src/main/java/de/fred4jupiter/fredbet/repository/UsernamePoints.java
+++ src/main/java/de/fred4jupiter/fredbet/repository/UsernamePoints.java
@@ -5,42 +5,53 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 
 public class UsernamePoints {
 
-	private String userName;
+    private String userName;
 
-	private Integer totalPoints;
+    private Integer totalPoints;
 
-	private String cssRankClass;
+    private Integer goalDifference;
 
-	public String getUserName() {
-		return userName;
-	}
+    private String cssRankClass;
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+    public Integer getGoalDifference() {
+        return goalDifference;
+    }
 
-	@Override
-	public String toString() {
-		ToStringBuilder builder = new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE);
-		builder.append("userName", userName);
-		builder.append("points", totalPoints);
-		return builder.toString();
-	}
+    public void setGoalDifference(Integer goalDifference) {
+        this.goalDifference = goalDifference;
+    }
 
-	public Integer getTotalPoints() {
-		return totalPoints;
-	}
+    public String getUserName() {
+        return userName;
+    }
 
-	public void setTotalPoints(Integer totalPoints) {
-		this.totalPoints = totalPoints;
-	}
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
-	public String getCssRankClass() {
-		return cssRankClass;
-	}
+    @Override
+    public String toString() {
+        ToStringBuilder builder = new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE);
+        builder.append("userName", userName);
+        builder.append("totalPoints", totalPoints);
+        builder.append("goalDifference", goalDifference);
+        return builder.toString();
+    }
 
-	public void setCssRankClass(String cssRankClass) {
-		this.cssRankClass = cssRankClass;
-	}
+    public Integer getTotalPoints() {
+        return totalPoints;
+    }
+
+    public void setTotalPoints(Integer totalPoints) {
+        this.totalPoints = totalPoints;
+    }
+
+    public String getCssRankClass() {
+        return cssRankClass;
+    }
+
+    public void setCssRankClass(String cssRankClass) {
+        this.cssRankClass = cssRankClass;
+    }
 
 }
--- src/main/java/de/fred4jupiter/fredbet/service/RankingService.java
+++ src/main/java/de/fred4jupiter/fredbet/service/RankingService.java
@@ -3,13 +3,16 @@ package de.fred4jupiter.fredbet.service;
 import de.fred4jupiter.fredbet.domain.RankingSelection;
 import de.fred4jupiter.fredbet.repository.BetRepository;
 import de.fred4jupiter.fredbet.repository.UsernamePoints;
+import de.fred4jupiter.fredbet.service.goaldiff.GoalsDifferenceCalculationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class RankingService {
@@ -20,22 +23,34 @@ public class RankingService {
     @Autowired
     private ChildRelationFetcher childRelationFetcher;
 
+    @Autowired
+    private GoalsDifferenceCalculationService goalsDifferenceCalculationService;
+
     public List<UsernamePoints> calculateCurrentRanking(RankingSelection rankingSelection) {
+        final List<UsernamePoints> rankings = betRepository.calculateRanging();
+
+        final Map<String, Integer> goalDifferenceForUsers = goalsDifferenceCalculationService.calculateGoalDifferenceForUsers();
+
+        rankings.forEach(usernamePoints -> {
+            Integer goalDiference = goalDifferenceForUsers.get(usernamePoints.getUserName());
+            usernamePoints.setGoalDifference(goalDiference);
+        });
+
+        final Map<String, Boolean> relationMap = childRelationFetcher.fetchUserIsChildRelation();
+        Stream<UsernamePoints> usernamePointsStream = rankings.stream().filter(Objects::nonNull);
+
         if (RankingSelection.MIXED.equals(rankingSelection)) {
-            return betRepository.calculateRanging();
-        }
-
-        Map<String, Boolean> relationMap = childRelationFetcher.fetchUserIsChildRelation();
-
-        final List<UsernamePoints> ranking = betRepository.calculateRanging();
-
-        if (RankingSelection.ONLY_ADULTS.equals(rankingSelection)) {
-            return ranking.stream().filter(Objects::nonNull).filter(usernamePoints -> !isChild(relationMap, usernamePoints)).collect(Collectors.toList());
-        } else if (RankingSelection.ONLY_CHILDREN.equals(rankingSelection)) {
-            return ranking.stream().filter(Objects::nonNull).filter(usernamePoints -> isChild(relationMap, usernamePoints)).collect(Collectors.toList());
+            // nothing to filter
+        } else if (RankingSelection.ONLY_ADULTS.equals(rankingSelection))
+            usernamePointsStream = usernamePointsStream.filter(usernamePoints -> !isChild(relationMap, usernamePoints));
+        else if (RankingSelection.ONLY_CHILDREN.equals(rankingSelection)) {
+            usernamePointsStream = usernamePointsStream.filter(usernamePoints -> isChild(relationMap, usernamePoints));
         } else {
             throw new IllegalArgumentException("Unsupported ranking selection " + rankingSelection);
         }
+
+        Comparator<UsernamePoints> comparator = Comparator.comparing(UsernamePoints::getTotalPoints).reversed().thenComparing(UsernamePoints::getGoalDifference);
+        return usernamePointsStream.sorted(comparator).collect(Collectors.toList());
     }
 
     private Boolean isChild(Map<String, Boolean> relationMap, UsernamePoints usernamePoints) {
