1d771a4549fae324aec4f51279fdf061bdbdbf4a
fred4jupiter <hamsterhase@gmx.de>
2019-03-03 11:53:16 +0100

    refactoring

--- src/main/java/de/fred4jupiter/fredbet/web/admin/ExcelExportController.java
+++ src/main/java/de/fred4jupiter/fredbet/web/admin/ExcelExportController.java
@@ -1,19 +1,14 @@
 package de.fred4jupiter.fredbet.web.admin;
 
-import javax.servlet.http.HttpServletResponse;
-
+import de.fred4jupiter.fredbet.security.FredBetPermission;
+import de.fred4jupiter.fredbet.service.excel.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import de.fred4jupiter.fredbet.security.FredBetPermission;
-import de.fred4jupiter.fredbet.service.excel.ReportService;
 
 @Controller
 @RequestMapping("/excelexport")
@@ -25,50 +20,50 @@ public class ExcelExportController {
     @Autowired
     private ReportService reportService;
 
-    @RequestMapping(value = "/show", method = RequestMethod.GET)
-    public ModelAndView showCachePage() {
-        return new ModelAndView("admin/excel_export");
+    @GetMapping("/show")
+    public String showCachePage() {
+        return "admin/excel_export";
     }
 
-    @RequestMapping(value = "/allBets", method = RequestMethod.GET, produces = CONTENT_TYPE_EXCEL)
-    public ResponseEntity<byte[]> exportAllBets(HttpServletResponse response) {
+    @GetMapping(value = "/allBets", produces = CONTENT_TYPE_EXCEL)
+    public ResponseEntity<byte[]> exportAllBets() {
         final String fileName = "allBets.xlsx";
         byte[] fileContent = this.reportService.exportBetsToExcel(LocaleContextHolder.getLocale());
         if (fileContent == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return ResponseEntity.notFound().build();
         }
 
         return createResponseEntity(fileName, fileContent);
     }
-    
-    @RequestMapping(value = "/extraBets", method = RequestMethod.GET, produces = CONTENT_TYPE_EXCEL)
-    public ResponseEntity<byte[]> exportExtraBets(HttpServletResponse response) {
+
+    @GetMapping(value = "/extraBets", produces = CONTENT_TYPE_EXCEL)
+    public ResponseEntity<byte[]> exportExtraBets() {
         final String fileName = "extraBets.xlsx";
         byte[] fileContent = this.reportService.exportExtraBetsToExcel(LocaleContextHolder.getLocale());
         if (fileContent == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return ResponseEntity.notFound().build();
         }
 
         return createResponseEntity(fileName, fileContent);
     }
 
-    @RequestMapping(value = "/pointsCount", method = RequestMethod.GET, produces = CONTENT_TYPE_EXCEL)
-    public ResponseEntity<byte[]> exportPointsCountByUser(HttpServletResponse response) {
+    @GetMapping(value = "/pointsCount", produces = CONTENT_TYPE_EXCEL)
+    public ResponseEntity<byte[]> exportPointsCountByUser() {
         final String fileName = "numberOfReachedPointsCount.xlsx";
         byte[] fileContent = this.reportService.exportNumberOfPointsInBets(LocaleContextHolder.getLocale());
         if (fileContent == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return ResponseEntity.notFound().build();
         }
 
         return createResponseEntity(fileName, fileContent);
     }
 
-    @RequestMapping(value = "/ranking", method = RequestMethod.GET, produces = CONTENT_TYPE_EXCEL)
-    public ResponseEntity<byte[]> exportRanking(HttpServletResponse response) {
+    @GetMapping(value = "/ranking", produces = CONTENT_TYPE_EXCEL)
+    public ResponseEntity<byte[]> exportRanking() {
         final String fileName = "ranking.xlsx";
         byte[] fileContent = this.reportService.exportRankingToExcel(LocaleContextHolder.getLocale());
         if (fileContent == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return ResponseEntity.notFound().build();
         }
 
         return createResponseEntity(fileName, fileContent);
