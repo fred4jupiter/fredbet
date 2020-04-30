c9deef2737f2d2f98fb58d5fbd645669169670e3
Staehler, Michael <michael.staehler@opitz-consulting.com>
2020-04-30 14:12:38 +0200

    showing detailed extra bets

--- src/main/java/de/fred4jupiter/fredbet/web/bet/ExtraBetCommandMapper.java
+++ src/main/java/de/fred4jupiter/fredbet/web/bet/ExtraBetCommandMapper.java
@@ -1,39 +1,40 @@
 package de.fred4jupiter.fredbet.web.bet;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import de.fred4jupiter.fredbet.domain.ExtraBet;
 import de.fred4jupiter.fredbet.domain.Match;
 import de.fred4jupiter.fredbet.service.BettingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Component
 public class ExtraBetCommandMapper {
 
-	@Autowired
-	private BettingService bettingService;
+    @Autowired
+    private BettingService bettingService;
 
-	public ExtraBetCommand toExtraBetCommand(ExtraBet extraBet) {
-		ExtraBetCommand extraBetCommand = new ExtraBetCommand();
-		Match finalMatch = bettingService.findFinalMatch();
-		if (finalMatch != null) {
-			extraBetCommand.setFinalMatch(finalMatch);
-		}
+    public ExtraBetCommand toExtraBetCommand(ExtraBet extraBet) {
+        ExtraBetCommand extraBetCommand = new ExtraBetCommand();
+        Match finalMatch = bettingService.findFinalMatch();
+        if (finalMatch != null) {
+            extraBetCommand.setFinalMatch(finalMatch);
+        }
 
-		extraBetCommand.setExtraBetId(extraBet.getId());
-		extraBetCommand.setFinalWinner(extraBet.getFinalWinner());
-		extraBetCommand.setSemiFinalWinner(extraBet.getSemiFinalWinner());
-		extraBetCommand.setThirdFinalWinner(extraBet.getThirdFinalWinner());
-		extraBetCommand.setPoints(extraBet.getPoints());
+        extraBetCommand.setExtraBetId(extraBet.getId());
+        extraBetCommand.setFinalWinner(extraBet.getFinalWinner());
+        extraBetCommand.setSemiFinalWinner(extraBet.getSemiFinalWinner());
+        extraBetCommand.setThirdFinalWinner(extraBet.getThirdFinalWinner());
+        extraBetCommand.setPointsOne(extraBet.getPointsOne());
+        extraBetCommand.setPointsTwo(extraBet.getPointsTwo());
+        extraBetCommand.setPointsThree(extraBet.getPointsThree());
 
-		boolean firstMatchStarted = bettingService.hasFirstMatchStarted();
-		if (firstMatchStarted) {
-			extraBetCommand.setBettable(false);
-		} else {
-			extraBetCommand.setBettable(true);
-		}
+        boolean firstMatchStarted = bettingService.hasFirstMatchStarted();
+        if (firstMatchStarted) {
+            extraBetCommand.setBettable(false);
+        } else {
+            extraBetCommand.setBettable(true);
+        }
 
-		return extraBetCommand;
-	}
+        return extraBetCommand;
+    }
 
 }
