59ff5317a4088c1b71aa0037e93abde40bfdbd06
fred4jupiter <hamsterhase@gmx.de>
2020-05-04 13:56:35 +0200

    cleanup

--- src/main/java/de/fred4jupiter/fredbet/web/WebMessageUtil.java
+++ src/main/java/de/fred4jupiter/fredbet/web/WebMessageUtil.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
@@ -34,11 +33,6 @@ public class WebMessageUtil {
         addMessage(redirect, CSS_ALT_SUCCESS, message);
     }
 
-    public void addInfoMsg(ModelMap modelMap, String msgKey, Object... params) {
-        String message = getMessageFor(msgKey, params);
-        modelMap.addAttribute(MSG_ATTRIBUTE_NAME, new WebMessage(CSS_ALT_SUCCESS, message));
-    }
-
     public void addInfoMsg(Model model, String msgKey, Object... params) {
         String message = getMessageFor(msgKey, params);
         model.addAttribute(MSG_ATTRIBUTE_NAME, new WebMessage(CSS_ALT_SUCCESS, message));
@@ -54,21 +48,6 @@ public class WebMessageUtil {
         model.addAttribute(MSG_ATTRIBUTE_NAME, new WebMessage(CSS_ALERT_ERROR, message));
     }
 
-    public void addErrorMsg(ModelMap modelMap, String msgKey, Object... params) {
-        String message = getMessageFor(msgKey, params);
-        modelMap.addAttribute(MSG_ATTRIBUTE_NAME, new WebMessage(CSS_ALERT_ERROR, message));
-    }
-
-    public void addWarnMsg(RedirectAttributes redirect, String msgKey, Object... params) {
-        String message = getMessageFor(msgKey, params);
-        addMessage(redirect, CSS_ALERT_WARN, message);
-    }
-
-    public void addWarnMsg(ModelMap modelMap, String msgKey, Object... params) {
-        String message = getMessageFor(msgKey, params);
-        modelMap.addAttribute(MSG_ATTRIBUTE_NAME, new WebMessage(CSS_ALERT_WARN, message));
-    }
-
     public void addWarnMsg(Model model, String msgKey, Object... params) {
         String message = getMessageFor(msgKey, params);
         model.addAttribute(MSG_ATTRIBUTE_NAME, new WebMessage(CSS_ALERT_WARN, message));
@@ -80,10 +59,6 @@ public class WebMessageUtil {
 
     public String getMessageFor(String msgKey, Object... params) {
         return messageSourceUtil.getMessageFor(msgKey, LocaleContextHolder.getLocale(), params);
-    }
-
-    public String getCountryName(Country country) {
-        return messageSourceUtil.getCountryName(country, LocaleContextHolder.getLocale());
     }
 
     public String getTeamNameOne(Match match) {
@@ -104,7 +79,6 @@ public class WebMessageUtil {
         private final String text;
 
         public WebMessage(String cssClass, String text) {
-            super();
             this.cssClass = cssClass;
             this.text = text;
         }
