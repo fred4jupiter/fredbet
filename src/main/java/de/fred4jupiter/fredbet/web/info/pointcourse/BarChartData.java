65d8068eedc3b3eabe3cd3817273198be8bd144a
fred4jupiter <hamsterhase@gmx.de>
2020-05-11 14:03:25 +0200

    loading data from controller

---
+++ src/main/java/de/fred4jupiter/fredbet/web/info/pointcourse/BarChartData.java
@@ -0,0 +1,36 @@
+package de.fred4jupiter.fredbet.web.info.pointcourse;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BarChartData {
+
+    private final List<String> labels = new ArrayList<>();
+
+    private final List<DataSet> datasets = new ArrayList<>();
+
+    public BarChartData(String... labels) {
+        this.labels.addAll(Arrays.asList(labels));
+    }
+
+    public void addDataSet(DataSet dataSet) {
+        this.datasets.add(dataSet);
+    }
+
+    public void addDataSet(String name, Integer... values) {
+        DataSet dataSet = new DataSet(name);
+        for (Integer value : values) {
+            dataSet.addData(value);
+        }
+        this.datasets.add(dataSet);
+    }
+
+    public List<String> getLabels() {
+        return labels;
+    }
+
+    public List<DataSet> getDatasets() {
+        return datasets;
+    }
+}
--- src/main/java/de/fred4jupiter/fredbet/web/info/pointcourse/PointCourseController.java
+++ src/main/java/de/fred4jupiter/fredbet/web/info/pointcourse/PointCourseController.java
@@ -1,5 +1,7 @@
 package de.fred4jupiter.fredbet.web.info.pointcourse;
 
+import de.fred4jupiter.fredbet.util.JsonObjectConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,12 @@ public class PointCourseController {
 
     @GetMapping("/show")
     public String showPage(Model model) {
+        BarChartData barChartData = new BarChartData("4 points", "3 points", "2 points", "1 points");
+        barChartData.addDataSet("admin", 2, 4, 3, 12);
+        barChartData.addDataSet("michael", 0, 1, 8, 9);
+        barChartData.addDataSet("test", 0, 3, 3, 13);
+
+        model.addAttribute("barChartData", barChartData);
         return "info/pointcourse";
     }
 }
