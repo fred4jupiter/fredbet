e606cc1da0f8b43d6b8ec7082dee59434753cba0
fred4jupiter <hamsterhase@gmx.de>
2020-12-23 13:18:12 +0100

    refactoring

--- src/main/resources/static/js/pages/image_upload.js
+++ src/main/resources/static/js/pages/image_upload.js
@@ -1,12 +1,19 @@
-function compressFile() {
-    const compress = new Compress()
+"use strict";
+const compressor = new Compress()
+const upload = document.getElementById('upload')
+const preview = document.getElementById('preview')
 
-    // or simpler
-    compress.attach('#myFile', {
+upload.addEventListener('change', function (evt) {
+    const files = [...evt.target.files]
+    compressor.compress(files, {
         size: 4,
         quality: .75
-    }).then((data) => {
-        $('#myFileBase64').val(data.data);
+    }).then((results) => {
+        console.log(results)
+        const output = results[0]
+        const file = Compress.convertBase64ToFile(output.data, output.ext)
+        console.log(file)
+        preview.src = output.prefix + output.data
+        $("#myFileBase64").attr("value", output.prefix + output.data);
     })
-}
-
+}, false)
