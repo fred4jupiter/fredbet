3e44883dfb6bf62e7d0424dcd69276dda2b2f01a
fred4jupiter <hamsterhase@gmx.de>
2020-12-30 14:20:37 +0100

    bugfix delete confirmation for matches

--- src/main/java/de/fred4jupiter/fredbet/web/admin/AdminFormCommand.java
+++ src/main/java/de/fred4jupiter/fredbet/web/admin/AdminFormCommand.java
@@ -1,10 +1,12 @@
 package de.fred4jupiter.fredbet.web.admin;
 
 import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
 public class AdminFormCommand {
 
+    @NotNull
     @Max(100)
     @Positive
     private Integer numberOfTestUsers;
