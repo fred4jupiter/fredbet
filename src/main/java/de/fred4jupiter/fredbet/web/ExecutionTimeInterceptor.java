fbea2dc78db22f70a9700fe4c9affabf6c2d60ba
fred4jupiter <hamsterhase@gmx.de>
2021-04-10 12:57:49 +0200

    update removing deprecation

--- src/main/java/de/fred4jupiter/fredbet/web/ExecutionTimeInterceptor.java
+++ src/main/java/de/fred4jupiter/fredbet/web/ExecutionTimeInterceptor.java
@@ -1,44 +1,46 @@
 package de.fred4jupiter.fredbet.web;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.logging.log4j.ThreadContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.AsyncHandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class ExecutionTimeInterceptor extends HandlerInterceptorAdapter {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-	private static final Logger LOG = LoggerFactory.getLogger(ExecutionTimeInterceptor.class);
+public class ExecutionTimeInterceptor implements AsyncHandlerInterceptor {
 
-	private static final String START_TIME_ATTRIBUTE_NAME = "startTime";
+    private static final Logger LOG = LoggerFactory.getLogger(ExecutionTimeInterceptor.class);
 
-	// before the actual handler will be executed
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		long startTime = System.currentTimeMillis();
-		request.setAttribute(START_TIME_ATTRIBUTE_NAME, startTime);
+    private static final String START_TIME_ATTRIBUTE_NAME = "startTime";
 
-		final String requestURI = request.getRequestURI();
+    // before the actual handler will be executed
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        long startTime = System.currentTimeMillis();
+        request.setAttribute(START_TIME_ATTRIBUTE_NAME, startTime);
 
-		ThreadContext.put("mdc.requestURI", requestURI);
-		return true;
-	}
+        final String requestURI = request.getRequestURI();
 
-	// after the handler is executed
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView)
-			throws Exception {
-		long startTime = (Long) request.getAttribute(START_TIME_ATTRIBUTE_NAME);
-		long endTime = System.currentTimeMillis();
-		long executeTime = endTime - startTime;
+        ThreadContext.put("mdc.requestURI", requestURI);
+        return true;
+    }
 
-		final String requestURI = request.getRequestURI();
+    // after the handler is executed
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView)
+            throws Exception {
+        long startTime = (Long) request.getAttribute(START_TIME_ATTRIBUTE_NAME);
+        long endTime = System.currentTimeMillis();
+        long executeTime = endTime - startTime;
 
-		ThreadContext.put("mdc.executionTime", "" + executeTime);
+        final String requestURI = request.getRequestURI();
 
-		LOG.debug("requestURI={}, executionTime={}", requestURI, executeTime);
+        ThreadContext.put("mdc.executionTime", "" + executeTime);
 
-		ThreadContext.clearMap();
-	}
+        LOG.debug("requestURI={}, executionTime={}", requestURI, executeTime);
+
+        ThreadContext.clearMap();
+    }
 }
