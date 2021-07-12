eb3e8b1d233a2e07a2909c4f739f119420fd8c35
Staehler, Michael <michael.staehler@opitz-consulting.com>
2021-07-12 20:06:22 +0200

    update tests

--- src/test/java/de/fred4jupiter/fredbet/domain/MatchUT.java
+++ src/test/java/de/fred4jupiter/fredbet/domain/MatchUT.java
@@ -43,6 +43,7 @@ public class MatchUT {
                 .build();
         assertThat(match).isNotNull();
         assertThat(match.isGroupMatch()).isTrue();
+        assertThat(match.isUndecidedResult()).isTrue();
         assertThat(match.isTeamOneWinner()).isFalse();
         assertThat(match.isTeamTwoWinner()).isFalse();
     }
@@ -73,6 +74,42 @@ public class MatchUT {
                 .build();
         assertThat(match).isNotNull();
         assertThat(match.isFinal()).isTrue();
+        assertThat(match.isGroupMatch()).isFalse();
+        assertThat(match.isTeamOneWinner()).isFalse();
+        assertThat(match.isTeamTwoWinner()).isTrue();
+        assertThat(match.isPenaltyWinnerOne()).isFalse();
+    }
+
+    @Test
+    public void winnerLooserWinnerOne() {
+        Match match = MatchBuilder.create()
+                .withTeams(Country.GERMANY, Country.ITALY)
+                .withGoals(1, 1)
+                .withPenaltyWinnerOne(true)
+                .withGroup(Group.FINAL)
+                .build();
+        assertThat(match).isNotNull();
+        assertThat(match.isFinal()).isTrue();
+        assertThat(match.getWinner()).isEqualTo(Country.GERMANY);
+        assertThat(match.getLooser()).isEqualTo(Country.ITALY);
+        assertThat(match.isGroupMatch()).isFalse();
+        assertThat(match.isTeamOneWinner()).isTrue();
+        assertThat(match.isTeamTwoWinner()).isFalse();
+        assertThat(match.isPenaltyWinnerOne()).isTrue();
+    }
+
+    @Test
+    public void winnerLooserWinnerTwo() {
+        Match match = MatchBuilder.create()
+                .withTeams(Country.GERMANY, Country.ITALY)
+                .withGoals(1, 1)
+                .withPenaltyWinnerOne(false)
+                .withGroup(Group.FINAL)
+                .build();
+        assertThat(match).isNotNull();
+        assertThat(match.isFinal()).isTrue();
+        assertThat(match.getWinner()).isEqualTo(Country.ITALY);
+        assertThat(match.getLooser()).isEqualTo(Country.GERMANY);
         assertThat(match.isGroupMatch()).isFalse();
         assertThat(match.isTeamOneWinner()).isFalse();
         assertThat(match.isTeamTwoWinner()).isTrue();
