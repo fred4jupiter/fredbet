6eeb68c346186a35f04f3790dfd97f461ac70608
Staehler, Michael <michael.staehler@opitz-consulting.com>
2021-07-22 14:56:42 +0200

    update test

--- src/test/java/de/fred4jupiter/fredbet/imexport/ImportExportServiceIT.java
+++ src/test/java/de/fred4jupiter/fredbet/imexport/ImportExportServiceIT.java
@@ -2,6 +2,10 @@ package de.fred4jupiter.fredbet.imexport;
 
 import de.fred4jupiter.fredbet.common.IntegrationTest;
 import de.fred4jupiter.fredbet.data.DatabasePopulator;
+import de.fred4jupiter.fredbet.repository.AppUserRepository;
+import de.fred4jupiter.fredbet.repository.BetRepository;
+import de.fred4jupiter.fredbet.repository.ExtraBetRepository;
+import de.fred4jupiter.fredbet.repository.MatchRepository;
 import de.fred4jupiter.fredbet.util.TempFileWriterUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +21,22 @@ public class ImportExportServiceIT {
     @Autowired
     private DatabasePopulator databasePopulator;
 
+    @Autowired
+    private AppUserRepository appUserRepository;
+
+    @Autowired
+    private MatchRepository matchRepository;
+
+    @Autowired
+    private BetRepository betRepository;
+
+    @Autowired
+    private ExtraBetRepository extraBetRepository;
+
     @Test
     public void exportAllAsJsonAndImportAgain() {
-        databasePopulator.createDemoUsers(10);
+        final int numberOfDemoUsers = 10;
+        databasePopulator.createDemoUsers(numberOfDemoUsers);
         databasePopulator.createRandomMatches();
         databasePopulator.createDemoBetsForAllUsers();
         databasePopulator.createDemoResultsForAllMatches();
@@ -30,5 +47,9 @@ public class ImportExportServiceIT {
 
         importExportService.importAllFromJson(json);
 
+        assertThat(appUserRepository.count()).isGreaterThanOrEqualTo(numberOfDemoUsers);
+        assertThat(matchRepository.count()).isGreaterThanOrEqualTo(48);
+        assertThat(betRepository.count()).isGreaterThanOrEqualTo(numberOfDemoUsers * 48);
+        assertThat(extraBetRepository.count()).isGreaterThanOrEqualTo(numberOfDemoUsers);
     }
 }
