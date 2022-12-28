1756af7f9ac17ed2bed1848d7fa7d68b2f2c57eb
fred4jupiter <hamsterhase@gmx.de>
2022-12-28 12:51:24 +0100

    refactoring security config

---
+++ src/main/java/de/fred4jupiter/fredbet/SecurityConfig.java
@@ -0,0 +1,82 @@
+package de.fred4jupiter.fredbet;
+
+import de.fred4jupiter.fredbet.props.FredBetProfile;
+import de.fred4jupiter.fredbet.security.FredBetPermission;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class SecurityConfig {
+
+    // 24 Stunden
+    private static final int REMEMBER_ME_TOKEN_VALIDITY_SECONDS = 24 * 60 * 60;
+
+    private final Environment environment;
+
+    public SecurityConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, PersistentTokenRepository persistentTokenRepository) throws Exception {
+        http.authorizeHttpRequests((authz) -> authz
+                .mvcMatchers("/actuator/**", "/webjars/**", "/favicon.ico", "/blueimpgallery/**",
+                        "/lightbox/**", "/static/**", "/css/**", "/fonts/**", "/images/**", "/js/**", "/login", "/logout", "/console/*", "/registration").permitAll()
+                .mvcMatchers("/user/**").hasAnyAuthority(FredBetPermission.PERM_USER_ADMINISTRATION)
+                .mvcMatchers("/admin/**", "/administration/**", "/h2/**").hasAnyAuthority(FredBetPermission.PERM_ADMINISTRATION)
+                .mvcMatchers("/buildinfo/**").hasAnyAuthority(FredBetPermission.PERM_SYSTEM_INFO)
+                .anyRequest().authenticated()
+        );
+        http.rememberMe((remember) -> remember.tokenRepository(persistentTokenRepository).tokenValiditySeconds(REMEMBER_ME_TOKEN_VALIDITY_SECONDS));
+        http.formLogin().loginPage("/login").defaultSuccessUrl("/matches/upcoming").failureUrl("/login?error=true");
+        http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").invalidateHttpSession(true).deleteCookies("JSESSIONID", "remember-me");
+
+        // disable cache control to allow usage of ETAG headers (no image reload if the image has not been changed)
+        http.headers().cacheControl().disable();
+        http.csrf().ignoringAntMatchers("/info/editinfo");
+        if (environment.acceptsProfiles(Profiles.of(FredBetProfile.DEV))) {
+            // this is for the embedded h2 console
+            http.headers().frameOptions().disable();
+
+            // otherwise the H2 console will not work
+            http.csrf().ignoringAntMatchers("/h2/*");
+        }
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
+        return new ServletListenerRegistrationBean<>(new HttpSessionEventPublisher());
+    }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository(DataSource dataSource) {
+        JdbcTokenRepositoryImpl jdbcTokenRepositoryImpl = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepositoryImpl.setDataSource(dataSource);
+        return jdbcTokenRepositoryImpl;
+    }
+
+}
