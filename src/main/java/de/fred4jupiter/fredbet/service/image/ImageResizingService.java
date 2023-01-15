8193674580ae660f08aefd74936891ded829baa9
fred4jupiter <hamsterhase@gmx.de>
2023-01-15 08:48:33 +0100

    update thumbailator; small refactorings; setting default thumbnail size to 100

--- src/main/java/de/fred4jupiter/fredbet/service/image/ImageResizingService.java
+++ src/main/java/de/fred4jupiter/fredbet/service/image/ImageResizingService.java
@@ -2,7 +2,6 @@ package de.fred4jupiter.fredbet.service.image;
 
 import de.fred4jupiter.fredbet.props.FredbetProperties;
 import net.coobird.thumbnailator.Thumbnails;
-import net.coobird.thumbnailator.Thumbnails.Builder;
 import net.coobird.thumbnailator.geometry.Positions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,7 +20,7 @@ import java.io.ByteArrayOutputStream;
 @Service
 class ImageResizingService {
 
-    private static final Logger log = LoggerFactory.getLogger(ImageResizingService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ImageResizingService.class);
 
     private final FredbetProperties fredbetProperties;
 
@@ -31,42 +30,26 @@ class ImageResizingService {
 
     public byte[] createThumbnail(byte[] imageBinary) {
         Integer thumbnailSize = fredbetProperties.getThumbnailSize();
-        return minimizeToSize(imageBinary, thumbnailSize, builder -> builder.crop(Positions.CENTER).size(thumbnailSize, thumbnailSize));
-    }
-
-    public byte[] minimizeToSize(byte[] imageBinary, int size) {
-        return minimizeToSize(imageBinary, size, builder -> builder.size(size, size));
-    }
-
-    public byte[] minimizeToSize(byte[] imageBinary, int size, ThumbnailsBuilderCallback byteArrayConverter) {
-        if (size == 0) {
-            throw new IllegalArgumentException("Given size must be greather than 0!");
+        if (thumbnailSize == 0) {
+            throw new IllegalArgumentException("Given thumbnailSize must be greather than 0!");
         }
         try (ByteArrayInputStream byteIn = new ByteArrayInputStream(imageBinary);
              ByteArrayOutputStream byteOut = new ByteArrayOutputStream()) {
 
             BufferedImage bufferedImage = ImageIO.read(byteIn);
-            if (bufferedImage.getWidth() <= size && bufferedImage.getHeight() <= size) {
-                log.info(
-                        "The original image does have the prefered size of {} or is smaller than that. Do not change image size. image with: {}, image height: {}",
-                        size, bufferedImage.getWidth(), bufferedImage.getHeight());
+            if (bufferedImage.getWidth() <= thumbnailSize && bufferedImage.getHeight() <= thumbnailSize) {
+                LOG.info(
+                        "The original image does have the prefered thumbnailSize of {} or is smaller than that. Do not change image thumbnailSize. image with: {}, image height: {}",
+                        thumbnailSize, bufferedImage.getWidth(), bufferedImage.getHeight());
                 return imageBinary;
             }
 
-            Builder<BufferedImage> builder = Thumbnails.of(bufferedImage);
-            byteArrayConverter.doWithBuilder(builder);
-
-            builder.outputFormat("JPEG").toOutputStream(byteOut);
+            Thumbnails.of(bufferedImage).crop(Positions.CENTER).size(thumbnailSize, thumbnailSize).outputFormat("JPEG").toOutputStream(byteOut);
             return byteOut.toByteArray();
         } catch (Exception e) {
-            log.error(e.getMessage(), e);
+            LOG.error(e.getMessage(), e);
             return null;
         }
     }
 
-    @FunctionalInterface
-    private interface ThumbnailsBuilderCallback {
-
-        void doWithBuilder(Builder<BufferedImage> builder);
-    }
 }
