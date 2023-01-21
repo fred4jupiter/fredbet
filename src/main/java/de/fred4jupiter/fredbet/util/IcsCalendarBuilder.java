e36de922ebf89e56386c2b07178b10ba359dfe5f
fred4jupiter <hamsterhase@gmx.de>
2023-01-21 13:38:40 +0100

    update spring boot to version 3.0.2; update ical4j

--- src/main/java/de/fred4jupiter/fredbet/util/IcsCalendarBuilder.java
+++ src/main/java/de/fred4jupiter/fredbet/util/IcsCalendarBuilder.java
@@ -1,6 +1,7 @@
 package de.fred4jupiter.fredbet.util;
 
 import net.fortuna.ical4j.data.CalendarOutputter;
+import net.fortuna.ical4j.model.Property;
 import net.fortuna.ical4j.model.TimeZone;
 import net.fortuna.ical4j.model.TimeZoneRegistry;
 import net.fortuna.ical4j.model.TimeZoneRegistryFactory;
@@ -17,6 +18,7 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.Optional;
 
 public class IcsCalendarBuilder {
 
@@ -73,21 +75,21 @@ public class IcsCalendarBuilder {
     }
 
     public byte[] build() {
+        VEvent vEvent = new VEvent(this.start, this.end, this.title);
+        vEvent.add(uidGenerator.generateUid());
+        vEvent.add(new Description(this.content));
+        vEvent.add(new Location(this.location));
+
+        // add timezone information..
         TimeZoneRegistry registry = TimeZoneRegistryFactory.getInstance().createRegistry();
         TimeZone timezone = registry.getTimeZone(timeZone);
         VTimeZone tz = timezone.getVTimeZone();
-        VEvent vEvent = new VEvent(toDate(this.start), toDate(this.end), this.title);
-        vEvent.getProperties().add(tz.getTimeZoneId());
-        vEvent.getProperties().add(uidGenerator.generateUid());
-        vEvent.getProperties().add(new Description(this.content));
-        vEvent.getProperties().add(new Location(this.location));
+        Optional<Property> property = tz.getProperty(Property.TZID);
+        property.ifPresent(vEvent::add);
 
         net.fortuna.ical4j.model.Calendar icsCalendar = new net.fortuna.ical4j.model.Calendar();
-        icsCalendar.getProperties().add(new ProdId("-//FredBet//iCal4j 1.0//EN"));
-        icsCalendar.getProperties().add(CalScale.GREGORIAN);
-        icsCalendar.getProperties().add(Version.VERSION_2_0);
-
-        icsCalendar.getComponents().add(vEvent);
+        icsCalendar.add(new ProdId("-//FredBet//iCal4j 1.0//EN"));
+        icsCalendar.add(vEvent);
 
         try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
             CalendarOutputter outputter = new CalendarOutputter();
