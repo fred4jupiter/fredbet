edb34262713f8424382903288fa106ba241a8086
fred4jupiter <hamsterhase@gmx.de>
2023-01-27 13:07:36 +0100

    refactoring config package

---
+++ src/main/java/de/fred4jupiter/fredbet/config/DefaultLocaleInitializer.java
@@ -0,0 +1,19 @@
+package de.fred4jupiter.fredbet.config;
+
+import de.fred4jupiter.fredbet.props.FredbetProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+public class DefaultLocaleInitializer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultLocaleInitializer.class);
+
+    public DefaultLocaleInitializer(FredbetProperties fredbetProperties) {
+        Locale.setDefault(fredbetProperties.getDefaultLocale());
+        LOG.info("Setting default locale to: {}", fredbetProperties.getDefaultLocale());
+    }
+}
