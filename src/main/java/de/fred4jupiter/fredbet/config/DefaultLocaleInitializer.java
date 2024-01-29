9a8e4cde063537e2cf9f0f57772651906859f61f
Staehler, Michael <michael.staehler@opitz-consulting.com>
2024-01-29 13:39:11 +0100

    refactoring

--- src/main/java/de/fred4jupiter/fredbet/config/DefaultLocaleInitializer.java
+++ src/main/java/de/fred4jupiter/fredbet/config/DefaultLocaleInitializer.java
@@ -3,16 +3,27 @@ package de.fred4jupiter.fredbet.config;
 import de.fred4jupiter.fredbet.props.FredbetProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.util.Locale;
 
 @Component
-public class DefaultLocaleInitializer {
+public class DefaultLocaleInitializer implements ApplicationListener<ApplicationReadyEvent> {
 
     private static final Logger LOG = LoggerFactory.getLogger(DefaultLocaleInitializer.class);
 
+    private final FredbetProperties fredbetProperties;
+
+    @Lazy
     public DefaultLocaleInitializer(FredbetProperties fredbetProperties) {
+        this.fredbetProperties = fredbetProperties;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
         Locale.setDefault(fredbetProperties.getDefaultLocale());
         LOG.info("Setting default locale to: {}", fredbetProperties.getDefaultLocale());
     }
---
+++ src/main/java/de/fred4jupiter/fredbet/config/ImageLocationConfig.java
@@ -0,0 +1,30 @@
+package de.fred4jupiter.fredbet.config;
+
+import de.fred4jupiter.fredbet.props.FredbetProperties;
+import de.fred4jupiter.fredbet.props.ImageLocation;
+import de.fred4jupiter.fredbet.repository.ImageBinaryRepository;
+import de.fred4jupiter.fredbet.service.image.storage.DatabaseImageLocationStrategy;
+import de.fred4jupiter.fredbet.service.image.storage.FilesystemImageLocationStrategy;
+import de.fred4jupiter.fredbet.service.image.storage.ImageLocationStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ImageLocationConfig {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ImageLocationConfig.class);
+
+    @Bean
+    public ImageLocationStrategy imageLocationStrategy(FredbetProperties fredbetProperties, ImageBinaryRepository imageBinaryRepository) {
+        final ImageLocation imageLocation = fredbetProperties.getImageLocation();
+
+        if (ImageLocation.FILE_SYSTEM.equals(imageLocation)) {
+            LOG.info("Storing images in filesystem.");
+            return new FilesystemImageLocationStrategy(fredbetProperties.getImageFileSystemBaseFolder());
+        }
+        LOG.info("Storing images in database.");
+        return new DatabaseImageLocationStrategy(imageBinaryRepository);
+    }
+}
