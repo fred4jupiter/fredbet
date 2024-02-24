39f56d2c352cf6666025f61ff372e979fdb117d5
fred4jupiter <hamsterhase@gmx.de>
2024-02-24 15:33:23 +0100

    cleanup

--- src/main/java/de/fred4jupiter/fredbet/repository/BetRepositoryImpl.java
+++ src/main/java/de/fred4jupiter/fredbet/repository/BetRepositoryImpl.java
@@ -1,8 +1,6 @@
 package de.fred4jupiter.fredbet.repository;
 
 import de.fred4jupiter.fredbet.props.FredbetConstants;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 
 import java.sql.ResultSet;
@@ -11,8 +9,6 @@ import java.util.List;
 import java.util.Map;
 
 class BetRepositoryImpl implements BetRepositoryCustom {
-
-    private static final Logger LOG = LoggerFactory.getLogger(BetRepositoryImpl.class);
 
     private final NamedParameterJdbcOperations namedParameterJdbcOperations;
 
@@ -40,7 +36,6 @@ class BetRepositoryImpl implements BetRepositoryCustom {
         Map<String, Object> params = new HashMap<>();
         params.put("username", FredbetConstants.TECHNICAL_USERNAME);
 
-        LOG.debug("sql={}", sql);
         return namedParameterJdbcOperations.query(sql, params, (ResultSet rs, int rowNum) -> {
             UsernamePoints usernamePoints = new UsernamePoints();
             usernamePoints.setUserName(rs.getString(1));
