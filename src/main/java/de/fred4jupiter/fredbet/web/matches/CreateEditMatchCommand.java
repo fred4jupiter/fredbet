03c5acbecca28d82aa42f60683cf716cb9042d48
fred4jupiter <hamsterhase@gmx.de>
2024-12-15 10:54:55 +0100

    update edit match

--- src/main/java/de/fred4jupiter/fredbet/web/matches/CreateEditMatchCommand.java
+++ src/main/java/de/fred4jupiter/fredbet/web/matches/CreateEditMatchCommand.java
@@ -1,129 +1,109 @@
 package de.fred4jupiter.fredbet.web.matches;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-import org.apache.commons.lang3.StringUtils;
-
 import de.fred4jupiter.fredbet.domain.Country;
 import de.fred4jupiter.fredbet.domain.Group;
 import de.fred4jupiter.fredbet.web.validation.ValidMatchConstraint;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
 
 @ValidMatchConstraint
 public class CreateEditMatchCommand {
 
-	private static final String DATE_FORMAT_PATTERN = "dd.MM.yyyy HH:mm";
+    private Long matchId;
 
-	private Long matchId;
+    private String teamNameOne;
+    private String teamNameTwo;
 
-	private String teamNameOne;
-	private String teamNameTwo;
+    @NotNull
+    private Country countryTeamOne;
 
-	@NotNull
-	private Country countryTeamOne;
+    @NotNull
+    private Country countryTeamTwo;
 
-	@NotNull
-	private Country countryTeamTwo;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    @NotNull
+    private LocalDateTime kickOffDate;
 
-	@NotNull
-	private LocalDateTime kickOffDate;
+    @NotNull
+    private Group group;
 
-	@NotNull
-	private Group group;
+    @NotBlank
+    private String stadium;
 
-	@NotBlank
-	private String stadium;
+    private boolean deletable;
 
-	private boolean deletable;
+    public Long getMatchId() {
+        return matchId;
+    }
 
-	public Long getMatchId() {
-		return matchId;
-	}
+    public void setMatchId(Long matchId) {
+        this.matchId = matchId;
+    }
 
-	public void setMatchId(Long matchId) {
-		this.matchId = matchId;
-	}
+    public String getTeamNameOne() {
+        return teamNameOne;
+    }
 
-	public String getTeamNameOne() {
-		return teamNameOne;
-	}
+    public void setTeamNameOne(String teamNameOne) {
+        this.teamNameOne = teamNameOne;
+    }
 
-	public void setTeamNameOne(String teamNameOne) {
-		this.teamNameOne = teamNameOne;
-	}
+    public String getTeamNameTwo() {
+        return teamNameTwo;
+    }
 
-	public String getTeamNameTwo() {
-		return teamNameTwo;
-	}
+    public void setTeamNameTwo(String teamNameTwo) {
+        this.teamNameTwo = teamNameTwo;
+    }
 
-	public void setTeamNameTwo(String teamNameTwo) {
-		this.teamNameTwo = teamNameTwo;
-	}
+    public Country getCountryTeamOne() {
+        return countryTeamOne;
+    }
 
-	public Country getCountryTeamOne() {
-		return countryTeamOne;
-	}
+    public void setCountryTeamOne(Country countryTeamOne) {
+        this.countryTeamOne = countryTeamOne;
+    }
 
-	public void setCountryTeamOne(Country countryTeamOne) {
-		this.countryTeamOne = countryTeamOne;
-	}
+    public Country getCountryTeamTwo() {
+        return countryTeamTwo;
+    }
 
-	public Country getCountryTeamTwo() {
-		return countryTeamTwo;
-	}
+    public void setCountryTeamTwo(Country countryTeamTwo) {
+        this.countryTeamTwo = countryTeamTwo;
+    }
 
-	public void setCountryTeamTwo(Country countryTeamTwo) {
-		this.countryTeamTwo = countryTeamTwo;
-	}
+    public LocalDateTime getKickOffDate() {
+        return kickOffDate;
+    }
 
-	public LocalDateTime getKickOffDate() {
-		return kickOffDate;
-	}
+    public void setKickOffDate(LocalDateTime kickOffDate) {
+        this.kickOffDate = kickOffDate;
+    }
 
-	public void setKickOffDate(LocalDateTime kickOffDate) {
-		this.kickOffDate = kickOffDate;
-	}
+    public Group getGroup() {
+        return group;
+    }
 
-	public Group getGroup() {
-		return group;
-	}
+    public void setGroup(Group group) {
+        this.group = group;
+    }
 
-	public void setGroup(Group group) {
-		this.group = group;
-	}
+    public String getStadium() {
+        return stadium;
+    }
 
-	public String getStadium() {
-		return stadium;
-	}
+    public void setStadium(String stadium) {
+        this.stadium = stadium;
+    }
 
-	public void setStadium(String stadium) {
-		this.stadium = stadium;
-	}
+    public boolean isDeletable() {
+        return deletable;
+    }
 
-	public String getKickOffDateString() {
-		if (kickOffDate == null) {
-			return "";
-		}
-		return kickOffDate.format(DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN));
-	}
-
-	public void setKickOffDateString(String kickOffDateString) {
-		if (StringUtils.isBlank(kickOffDateString)) {
-			return;
-		}
-		this.kickOffDate = LocalDateTime.parse(kickOffDateString, DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN));
-	}
-
-	public boolean isDeletable() {
-		return deletable;
-	}
-
-	public void setDeletable(boolean deletable) {
-		this.deletable = deletable;
-	}
-	
-	
+    public void setDeletable(boolean deletable) {
+        this.deletable = deletable;
+    }
 }
