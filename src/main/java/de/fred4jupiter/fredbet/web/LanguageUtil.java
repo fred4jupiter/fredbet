843d4468f7709fbaeffca8a274ff03c455e84325
fred4jupiter <hamsterhase@gmx.de>
2024-12-27 08:02:09 +0100

    added alt attribute

--- src/main/java/de/fred4jupiter/fredbet/web/LanguageUtil.java
+++ src/main/java/de/fred4jupiter/fredbet/web/LanguageUtil.java
@@ -2,25 +2,23 @@ package de.fred4jupiter.fredbet.web;
 
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
-
-import de.fred4jupiter.fredbet.domain.Country;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @Component
 public class LanguageUtil {
 
-	private static final String LANGUAGE_ICON_BASE_PATH = "/images/lang/";
+    private static final String LANGUAGE_ICON_BASE_PATH = "/images/lang/";
 
-	public String getCurrentUserLanguage() {
-		return LocaleContextHolder.getLocale().getLanguage();
-	}
+    public String getCurrentUserLanguage() {
+        return LocaleContextHolder.getLocale().getLanguage();
+    }
 
-	public String getImageLanguageIconPath() {
-		return LANGUAGE_ICON_BASE_PATH + getCurrentUserLanguage() + ".png";
-	}
+    public String getImageLanguageIconPath() {
+        return LANGUAGE_ICON_BASE_PATH + getCurrentUserLanguage() + ".png";
+    }
 
-	public String buildUrl(String languageCode) {
-		ServletUriComponentsBuilder builder = ServletUriComponentsBuilder.fromCurrentRequest();
-		return builder.replaceQueryParam("lang", languageCode).toUriString();
-	}
+    public String buildUrl(String languageCode) {
+        ServletUriComponentsBuilder builder = ServletUriComponentsBuilder.fromCurrentRequest();
+        return builder.replaceQueryParam("lang", languageCode).toUriString();
+    }
 }
