78849a762219e29a16c2b7bb9d985e9be6d7bf95
Staehler, Michael <michael.staehler@opitz-consulting.com>
2025-01-06 12:37:24 +0100

    refactoring language selection

--- src/main/java/de/fred4jupiter/fredbet/web/LanguageUtil.java
+++ src/main/java/de/fred4jupiter/fredbet/web/LanguageUtil.java
@@ -1,21 +1,10 @@
 package de.fred4jupiter.fredbet.web;
 
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @Component
 public class LanguageUtil {
-
-    private static final String LANGUAGE_ICON_BASE_PATH = "/images/lang/";
-
-    public String getCurrentUserLanguage() {
-        return LocaleContextHolder.getLocale().getLanguage();
-    }
-
-    public String getImageLanguageIconPath() {
-        return LANGUAGE_ICON_BASE_PATH + getCurrentUserLanguage() + ".png";
-    }
 
     public String buildUrl(String languageCode) {
         ServletUriComponentsBuilder builder = ServletUriComponentsBuilder.fromCurrentRequest();
