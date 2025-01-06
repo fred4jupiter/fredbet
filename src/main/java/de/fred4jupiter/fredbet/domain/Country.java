d1538c4b2e9953eabc1838d93503f539b1f79458
Staehler, Michael <michael.staehler@opitz-consulting.com>
2025-01-06 10:01:42 +0100

    refactoring flags

--- src/main/java/de/fred4jupiter/fredbet/domain/Country.java
+++ src/main/java/de/fred4jupiter/fredbet/domain/Country.java
@@ -9,84 +9,97 @@ public enum Country {
 
     NONE("none"), // non selected country placeholder
 
-    ALBANIA("alb"), BELGIUM("bel"), GERMANY("deu"), GREAT_BRITAIN("gbr"), ENGLAND("eng"), FRANCE("fra"),
-    IRELAND("irl"), ICELAND("isl"), ITALY("ita"), CROATIA("hrv"), NORTH_IRELAND("north_ireland"),
+    ALBANIA("alb"), BELGIUM("bel"), GERMANY("deu"), GREAT_BRITAIN("gbr"), ENGLAND("eng", "gb-eng"), FRANCE("fra"),
+    IRELAND("irl"), ICELAND("isl"), ITALY("ita"), CROATIA("hrv"),
+    NORTH_IRELAND("north_ireland", "gb-nir"),
     AUSTRIA("aut"), POLAND("pol"), PORTUGAL("prt"), ROMANIA("rou"), RUSSIA("rus"),
     SWEDEN("swe"), SWITZERLAND("che"),
     SLOVAKIA("svk"), SPAIN("esp"), CZECH_REPUBLIC("cze"), TURKEY("tur"), UKRAINE("ukr"),
 
-    HUNGARY("hun"), WALES("wales"), AUSTRALIA("aus"), EGYPT("egy"), ALGERIA("alg"), ANGOLA("ang"), EQUATORIAL_GUINEA("eqg"),
+    HUNGARY("hun"), WALES("wales", "gb-wls"), AUSTRALIA("aus"), EGYPT("egy"), ALGERIA("dza"), ANGOLA("ago"), EQUATORIAL_GUINEA("gnq"),
 
     ETHIOPIA("eth"), BENIN("ben"), BURKINA_FASO("bfa"), BURUNDI("bdi"), DJIBOUTI("dji"), IVORY_COAST("civ"),
 
-    ERITREA("eri"), GABON("gab"), GAMBIA("gam"), GHANA("gha"), GUINEA("gui"), GUINEA_BISSEAU("gnb"), BOTSWANA("bot"),
+    ERITREA("eri"), GABON("gab"), GAMBIA("gmb"), GHANA("gha"), GUINEA("gin"), GUINEA_BISSEAU("gnb"), BOTSWANA("bwa"),
 
-    CAMEROON("cmr"), CAPE_VERDE_ISLANDS("cpv"), CENTRAL_AFRICAN_REPUBLIC("cta"), CHAD("cha"), COMOROS("com"), CONGO("cgo"),
+    CAMEROON("cmr"), CAPE_VERDE_ISLANDS("cpv"), CENTRAL_AFRICAN_REPUBLIC("caf"), CHAD("tcd"), COMOROS("com"), CONGO("cod"),
 
-    CONGO_DR("cod"), KENYA("ken"), LESOTHO("les"), LIBERIA("lbr"), LIBYA("lby"), MADAGASCAR("mad"),
+    CONGO_DR("cod"), KENYA("ken"), LESOTHO("lso"), LIBERIA("lbr"), LIBYA("lby"), MADAGASCAR("mdg"),
 
-    MALAWI("mwi"), MALI("mli"), MAURITANIA("mtn"), MAURITIUS("mus"),
+    MALAWI("mwi"), MALI("mli"), MAURITANIA("mrt"), MAURITIUS("mus"),
 
-    MOROCCO("mar"), MOZAMBIQUE("moz"), NAMIBIA("nam"), NIGER("nig"), NIGERIA("nga"), RWANDA("rwa"), SAO_TOME_E_PRINCIPE("stp"),
+    MOROCCO("mar"), MOZAMBIQUE("moz"), NAMIBIA("nam"), NIGER("ner"), NIGERIA("nga"), RWANDA("rwa"), SAO_TOME_E_PRINCIPE("stp"),
 
-    SENEGAL("sen"), SEYCHELLES("sey"), SIERRA_LEONE("sle"), SOMALIA("som"), SOUTH_AFRICA("rsa"), SOUTH_SUDAN("ssd"), SUDAN("sdn"),
+    SENEGAL("sen"), SEYCHELLES("syc"), SIERRA_LEONE("sle"), SOMALIA("som"), SOUTH_AFRICA("zaf"), SOUTH_SUDAN("ssd"), SUDAN("sdn"),
 
-    SWAZILAND("swz"), TANZANIA("tan"),
+    SWAZILAND("swz"), TANZANIA("tza"),
 
-    TOGO("tog"), TUNISIA("tun"), UGANDA("uga"), ZAMBIA("zam"), AFGHANISTAN("afg"), BAHRAIN("bhr"), BANGLADESH("ban"), BHUTAN("bhu"),
+    TOGO("tgo"), TUNISIA("tun"), UGANDA("uga"), ZAMBIA("zmb"), AFGHANISTAN("afg"), BAHRAIN("bhr"), BANGLADESH("bgd"), BHUTAN("btn"),
 
-    BRUNEI_DARUSSALAM("bru"), CAMBODIA("cam"), CHINA_PR("chn"), CHINESE_TAIPEI("tpe"), GUAM("gum"), HONG_KONG("hkg"), INDIA("ind"),
+    BRUNEI_DARUSSALAM("brn"), CAMBODIA("khm"), CHINA_PR("chn"), CHINESE_TAIPEI("twn"), GUAM("gum"), HONG_KONG("hkg"), INDIA("ind"),
 
-    IRAN("irn"), IRAQ("irq"), JAPAN("jpn"), JORDAN("jor"), KOREA_DPR("prk"), KOREA_REPUBLIC("kor"), KUWAIT("kuw"),
+    IRAN("irn"), IRAQ("irq"), JAPAN("jpn"), JORDAN("jor"), KOREA_DPR("prk"), KOREA_REPUBLIC("kor"), KUWAIT("kwt"),
 
-    KYRGYZSTAN("kgz"), LAOS("lao"), LEBANON("lib"), MACAU("mac"), MALAYSIA("mas"), MALDIVES("mdv"), MONGOLIA("mng"), MYANMAR("mya"),
+    KYRGYZSTAN("kgz"), LAOS("lao"), LEBANON("lbn"), MACAU("mac"), MALAYSIA("mys"), MALDIVES("mdv"), MONGOLIA("mng"), MYANMAR("mmr"),
 
-    NEPAL("nep"), OMAN("oma"), PAKISTAN("pak"), PALESTINE("ple"), PHILIPPINES("phi"), QATAR("qat"), SAUDI_ARABIA("ksa"),
+    NEPAL("npl"), OMAN("omn"), PAKISTAN("pak"), PALESTINE("pse"), PHILIPPINES("phl"), QATAR("qat"), SAUDI_ARABIA("sau"),
 
-    SINGAPORE("sin"), SRI_LANKA("sri"), SYRIA("syr"), TAJIKISTAN("tjk"), THAILAND("tha"),
+    SINGAPORE("sgp"), SRI_LANKA("lka"), SYRIA("syr"), TAJIKISTAN("tjk"), THAILAND("tha"),
 
-    TIMOR_LESTE("tls"), TURKMENISTAN("tkm"), UNITED_ARAB_EMIRATES("uae"), UZBEKISTAN("uzb"), VIETNAM("vie"), YEMEN("yem"), ANDORRA("and"),
+    TIMOR_LESTE("tls"), TURKMENISTAN("tkm"), UNITED_ARAB_EMIRATES("are"), UZBEKISTAN("uzb"), VIETNAM("vnm"), YEMEN("yem"), ANDORRA("and"),
 
-    ARMENIA("arm"), AZERBAIJAN("aze"), BELARUS("blr"), BOSNIA_HERZEGOVINA("bih"), BULGARIA("bul"), CYPRUS("cyp"), DENMARK("den"), ESTONIA(
-            "est"),
+    ARMENIA("arm"), AZERBAIJAN("aze"), BELARUS("blr"), BOSNIA_HERZEGOVINA("bih"), BULGARIA("bgr"), CYPRUS("cyp"), DENMARK("dnk"), ESTONIA(
+        "est"),
 
-    FAROE_ISLANDS("fro"), FINLAND("fin"), MACEDONIA("mkd"), GEORGIA("geo"), GIBRALTAR("gib"), GREECE("gre"), ISRAEL("isr"), KAZAKHSTAN(
-            "kaz"),
+    FAROE_ISLANDS("fro"), FINLAND("fin"), MACEDONIA("mkd"), GEORGIA("geo"), GIBRALTAR("gib"), GREECE("grc"), ISRAEL("isr"), KAZAKHSTAN(
+        "kaz"),
 
-    KOSOVO("kvx"), LATVIA("lva"), LIECHTENSTEIN("lie"), LITHUANIA("ltu"), LUXEMBOURG("lux"), MALTA("mlt"), MOLDOVA("mda"), MONTENEGRO(
-            "mne"),
+    LATVIA("lva"), LIECHTENSTEIN("lie"), LITHUANIA("ltu"), LUXEMBOURG("lux"), MALTA("mlt"), MOLDOVA("mda"), MONTENEGRO(
+        "mne"),
 
-    NETHERLANDS("ned"), NORWAY("nor"), SAN_MARINO("smr"), SCOTLAND("sco"), SERBIA("srb"), SLOVENIA("svn"),
+    NETHERLANDS("nld"), NORWAY("nor"), SAN_MARINO("smr"), SCOTLAND("sco", "gb-sct"), SERBIA("srb"), SLOVENIA("svn"),
 
-    ANGUILLA("aia"), ANTIGUA_AND_BARBUDA("atg"), ARUBA("aru"), BAHAMAS("bah"), BARBADOS("brb"), BELIZE("blz"), BERMUDA("ber"),
+    ANGUILLA("aia"), ANTIGUA_AND_BARBUDA("atg"), ARUBA("abw"), BAHAMAS("bhs"), BARBADOS("brb"), BELIZE("blz"), BERMUDA("bmu"),
 
-    BRITISH_VIRGIN_ISLANDS("vgb"), CANADA("can"), CAYMAN_ISLANDS("cay"), COSTA_RICA("crc"), CUBA("cub"), CURACAO("cuw"), DOMINICA("dma"),
+    BRITISH_VIRGIN_ISLANDS("vgb"), CANADA("can"), CAYMAN_ISLANDS("cym"), COSTA_RICA("cri"), CUBA("cub"), CURACAO("cuw"), DOMINICA("dma"),
 
-    DOMINICAN_REPUBLIC("dom"), EL_SALVADOR("slv"), GRENADA("grn"), GUATEMALA("gua"), GUYANA("guy"), HAITI("hai"), HONDURAS("hon"), JAMAICA(
-            "jam"),
+    DOMINICAN_REPUBLIC("dom"), EL_SALVADOR("slv"), GRENADA("grd"), GUATEMALA("gtm"), GUYANA("guy"), HAITI("hti"), HONDURAS("hnd"), JAMAICA(
+        "jam"),
 
-    MEXICO("mex"), MONTSERRAT("msr"), NICARAGUA("nca"), PANAMA("pan"), PUERTO_RICO("pur"), ST_KITTS_AND_NEVIS("skn"), ST_LUCIA("lca"),
+    MEXICO("mex"), MONTSERRAT("msr"), NICARAGUA("nic"), PANAMA("pan"), PUERTO_RICO("pri"), ST_KITTS_AND_NEVIS("kna"), ST_LUCIA("lca"),
 
-    ST_VINCENT_GRENADINES("vin"), SURINAME("sur"), TRINIDAD_AND_TOBAGO("tri"), TURKS_AND_CAICOS_ISLANDS("tca"), US_VIRGIN_ISLANDS("vir"),
+    ST_VINCENT_GRENADINES("vct"), SURINAME("sur"), TRINIDAD_AND_TOBAGO("tto"), TURKS_AND_CAICOS_ISLANDS("tca"), US_VIRGIN_ISLANDS("vir"),
 
-    USA("usa"), AMERICAN_SAMOA("asa"), COOK_ISLANDS("cok"), FIJI("fij"), NEW_CALEDONIA("ncl"), NEW_ZEALAND("nzl"), PAPUA_NEW_GUINEA("png"),
+    USA("usa"), AMERICAN_SAMOA("wsm"), COOK_ISLANDS("cok"), FIJI("fji"), NEW_CALEDONIA("ncl"), NEW_ZEALAND("nzl"), PAPUA_NEW_GUINEA("png"),
 
-    SAMOA("sam"), SOLOMON_ISLANDS("sol"), TAHITI("tah"), TONGA("tga"), VANUATU("van"), ARGENTINA("arg"), BOLIVIA("bol"), BRAZIL("bra"),
+    SAMOA("wsm"), SOLOMON_ISLANDS("slb"), TAHITI("pyf"), TONGA("ton"), VANUATU("vut"), ARGENTINA("arg"), BOLIVIA("bol"), BRAZIL("bra"),
 
-    CHILE("chi"), COLOMBIA("col"), ECUADOR("ecu"), PARAGUAY("par"), PERU("per"), URUGUAY("uru"), VENEZUELA("ven"), CATALAN("cat");
+    CHILE("chl"), COLOMBIA("col"), ECUADOR("ecu"), PARAGUAY("pry"), PERU("per"), URUGUAY("ury"), VENEZUELA("ven");
 
     public static final String ICON_BASE_PATH = "/images/flags/";
 
     private static final String ICON_BASE_PATH_BIG = "/images/flags/42_28/";
 
-    private final String isoCode;
+    private final String alpha3Code;
 
-    Country(String isoCode) {
-        this.isoCode = isoCode;
+    private final String flagIconCode;
+
+    Country(String alpha3Code) {
+        this.alpha3Code = alpha3Code;
+        this.flagIconCode = null;
     }
 
-    public String getIsoCode() {
-        return isoCode;
+    Country(String alpha3Code, String flagIconCode) {
+        this.alpha3Code = alpha3Code;
+        this.flagIconCode = flagIconCode;
+    }
+
+    public String getAlpha3Code() {
+        return alpha3Code;
+    }
+
+    public String getFlagIconCode() {
+        return flagIconCode;
     }
 
     public static Country fromName(String name) {
@@ -99,27 +112,17 @@ public enum Country {
         return null;
     }
 
-    public static Country fromIsoCode(String isoCountryCode) {
-        for (Country country : values()) {
-            if (country.getIsoCode().equalsIgnoreCase(isoCountryCode)) {
-                return country;
-            }
-        }
-
-        return null;
-    }
-
     public String getIconPath() {
         if (Country.NONE.equals(this)) {
             return null;
         }
-        return ICON_BASE_PATH + isoCode + ".png";
+        return ICON_BASE_PATH + alpha3Code + ".png";
     }
 
     public String getIconPathBig() {
         if (Country.NONE.equals(this)) {
             return null;
         }
-        return ICON_BASE_PATH_BIG + isoCode + ".png";
+        return ICON_BASE_PATH_BIG + alpha3Code + ".png";
     }
 }
