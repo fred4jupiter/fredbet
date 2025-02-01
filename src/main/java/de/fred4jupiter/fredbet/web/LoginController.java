c8427a0795d4b55b7ffa4a25c585a9038a1f1f11
fred4jupiter <hamsterhase@gmx.de>
2025-02-01 12:32:06 +0100

    update form login hanlding

--- src/main/java/de/fred4jupiter/fredbet/web/LoginController.java
+++ src/main/java/de/fred4jupiter/fredbet/web/LoginController.java
@@ -6,10 +6,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+
+    private final WebMessageUtil webMessageUtil;
+
+    public LoginController(WebMessageUtil webMessageUtil) {
+        this.webMessageUtil = webMessageUtil;
+    }
 
     @GetMapping
     public String loginPage() {
@@ -17,13 +24,13 @@ public class LoginController {
     }
 
     @GetMapping("/error")
-    public String loginError(Model model, HttpServletRequest httpServletRequest) {
+    public String loginError(Model model, HttpServletRequest httpServletRequest, RedirectAttributes redirect) {
         Object authException = httpServletRequest.getSession().getAttribute("SPRING_SECURITY_LAST_EXCEPTION");
         if (authException instanceof AuthenticationException authenticationException) {
-            model.addAttribute("errorMsg", authenticationException.getLocalizedMessage());
+            webMessageUtil.addErrorMsg(redirect, "login.error", authenticationException.getLocalizedMessage());
         }
 
         model.addAttribute("loginError", true);
-        return "login";
+        return "redirect:/login";
     }
 }
