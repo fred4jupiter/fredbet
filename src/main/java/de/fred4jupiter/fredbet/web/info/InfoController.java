901346053983282ca3b1a42958058369ab894988
Staehler, Michael <michael.staehler@opitz-consulting.com>
2025-05-14 12:25:34 +0200

    export infos as pdf

--- src/main/java/de/fred4jupiter/fredbet/web/info/InfoController.java
+++ src/main/java/de/fred4jupiter/fredbet/web/info/InfoController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.util.HtmlUtils;
 
 import java.time.format.DateTimeFormatter;
 import java.util.List;
