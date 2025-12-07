750928fad03dfc194cce888b37015ec76b256307
fred4jupiter <hamsterhase@gmx.de>
2025-12-07 07:57:04 +0100

    migrate to spring boot 4.0.0

--- src/main/java/de/fred4jupiter/fredbet/config/SecurityConfig.java
+++ src/main/java/de/fred4jupiter/fredbet/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package de.fred4jupiter.fredbet.config;
 
 import de.fred4jupiter.fredbet.security.FredBetPermission;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.h2console.autoconfigure.H2ConsoleProperties;
+import org.springframework.boot.security.autoconfigure.web.servlet.PathRequest;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +19,7 @@ import org.springframework.security.web.authentication.rememberme.PersistentToke
 import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 import javax.sql.DataSource;
+import java.util.Optional;
 
 @Configuration
 @EnableWebSecurity
@@ -26,15 +29,16 @@ public class SecurityConfig {
     // 24 Stunden
     private static final int REMEMBER_ME_TOKEN_VALIDITY_SECONDS = 24 * 60 * 60;
 
-    private final boolean h2Enabled;
+    private final Optional<H2ConsoleProperties> h2ConsoleProperties;
 
-    public SecurityConfig(@Value("${spring.h2.console.enabled}") boolean h2Enabled) {
-        this.h2Enabled = h2Enabled;
+    public SecurityConfig(Optional<H2ConsoleProperties> h2ConsoleProperties) {
+        this.h2ConsoleProperties = h2ConsoleProperties;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, PersistentTokenRepository persistentTokenRepository) throws Exception {
         http.authorizeHttpRequests(authz -> authz
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // see StaticResourceLocation for default static resource mappings
                 .requestMatchers("/static/**", "/actuator/**", "/blueimpgallery/**", "/lightbox/**", "/flag-icons*/**",
                     "/club-wm-icons*/**", "/fonts/**", "/login/**", "/logout", "/registration", "/console/",
                     "/webjars/**", "/css/**", "/js/**").permitAll()
@@ -54,7 +58,7 @@ public class SecurityConfig {
             // disable cache control to allow usage of ETAG headers (no image reload if the image has not been changed)
             .headers(headers -> headers.cacheControl(HeadersConfigurer.CacheControlConfig::disable));
 
-        if (h2Enabled) {
+        if (h2ConsoleProperties.isPresent() && h2ConsoleProperties.get().isEnabled()) {
             // this is for the embedded h2 console
             http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
             http.csrf(csrf -> csrf.ignoringRequestMatchers("/console/**"));
--- src/main/java/de/fred4jupiter/fredbet/web/WebSecurityUtil.java
+++ src/main/java/de/fred4jupiter/fredbet/web/WebSecurityUtil.java
@@ -8,8 +8,11 @@ import de.fred4jupiter.fredbet.props.FredbetProperties;
 import de.fred4jupiter.fredbet.security.FredBetPermission;
 import de.fred4jupiter.fredbet.security.SecurityService;
 import de.fred4jupiter.fredbet.settings.RuntimeSettingsService;
+import org.springframework.boot.h2console.autoconfigure.H2ConsoleProperties;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 /**
  * Security utility class for access in Thymeleaf templates.
@@ -25,14 +28,17 @@ public class WebSecurityUtil {
 
     private final ImageAdministrationService imageAdministrationService;
 
+    private final Optional<H2ConsoleProperties> h2ConsoleProperties;
+
     private final String adminUsername;
 
     public WebSecurityUtil(RuntimeSettingsService runtimeSettingsService, SecurityService securityService,
                            ImageAdministrationService imageAdministrationService,
-                           FredbetProperties fredbetProperties) {
+                           Optional<H2ConsoleProperties> h2ConsoleProperties, FredbetProperties fredbetProperties) {
         this.runtimeSettingsService = runtimeSettingsService;
         this.securityService = securityService;
         this.imageAdministrationService = imageAdministrationService;
+        this.h2ConsoleProperties = h2ConsoleProperties;
         this.adminUsername = fredbetProperties.adminUsername();
     }
 
@@ -93,7 +99,7 @@ public class WebSecurityUtil {
     }
 
     public String resolveH2Path() {
-        return "/console"; // TODO: read from config
+        return h2ConsoleProperties.map(H2ConsoleProperties::getPath).orElse("");
     }
 
     public String getCurrentUserTheme() {
