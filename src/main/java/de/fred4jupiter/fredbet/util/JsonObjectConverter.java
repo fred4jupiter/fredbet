0dd3a5de6ba92d81b0b45e81e1d214eada503b3c
fred4jupiter <hamsterhase@gmx.de>
2026-01-30 16:25:06 +0100

    cleanup

--- src/main/java/de/fred4jupiter/fredbet/util/JsonObjectConverter.java
+++ src/main/java/de/fred4jupiter/fredbet/util/JsonObjectConverter.java
@@ -1,14 +1,10 @@
 package de.fred4jupiter.fredbet.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import tools.jackson.databind.ObjectMapper;
 
 @Component
 public class JsonObjectConverter {
-
-    private static final Logger LOG = LoggerFactory.getLogger(JsonObjectConverter.class);
 
     private final ObjectMapper objectMapper;
 
