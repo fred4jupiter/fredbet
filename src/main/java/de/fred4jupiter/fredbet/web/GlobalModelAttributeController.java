0d8cbc3960532446f6104060fa81a670d7fb58df
Michael Staehler <michael.staehler@opitz-consulting.com>
2026-06-12 11:49:23 +0200

    update spring boot to 4.0.6 and introduced controller advice for model attribute beans

---
+++ src/main/java/de/fred4jupiter/fredbet/web/GlobalModelAttributeController.java
@@ -0,0 +1,83 @@
+package de.fred4jupiter.fredbet.web;
+
+import de.fred4jupiter.fredbet.util.MessageSourceUtil;
+import de.fred4jupiter.fredbet.web.bet.BettingWebUtil;
+import de.fred4jupiter.fredbet.web.util.LanguageIconUtil;
+import de.fred4jupiter.fredbet.web.util.TeamUtil;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+/**
+ * Global model attribute controller for adding utility beans to all templates.
+ * This avoids the Thymeleaf bean access restriction in Spring Boot 4.0.
+ */
+@ControllerAdvice
+public class GlobalModelAttributeController {
+
+    private final WebSecurityUtil webSecurityUtil;
+    private final RuntimeSettingsUtil runtimeSettingsUtil;
+    private final MessageSourceUtil messageSourceUtil;
+    private final TeamUtil teamUtil;
+    private final GroupAvailabilityUtil groupAvailabilityUtil;
+    private final LanguageIconUtil languageIconUtil;
+    private final LanguageUtil languageUtil;
+    private final BettingWebUtil bettingWebUtil;
+
+    public GlobalModelAttributeController(WebSecurityUtil webSecurityUtil,
+                                         RuntimeSettingsUtil runtimeSettingsUtil,
+                                         MessageSourceUtil messageSourceUtil,
+                                         TeamUtil teamUtil,
+                                         GroupAvailabilityUtil groupAvailabilityUtil,
+                                         LanguageIconUtil languageIconUtil,
+                                         LanguageUtil languageUtil,
+                                         BettingWebUtil bettingWebUtil) {
+        this.webSecurityUtil = webSecurityUtil;
+        this.runtimeSettingsUtil = runtimeSettingsUtil;
+        this.messageSourceUtil = messageSourceUtil;
+        this.teamUtil = teamUtil;
+        this.groupAvailabilityUtil = groupAvailabilityUtil;
+        this.languageIconUtil = languageIconUtil;
+        this.languageUtil = languageUtil;
+        this.bettingWebUtil = bettingWebUtil;
+    }
+
+    @ModelAttribute("webSecurityUtil")
+    public WebSecurityUtil webSecurityUtil() {
+        return webSecurityUtil;
+    }
+
+    @ModelAttribute("runtimeSettingsUtil")
+    public RuntimeSettingsUtil runtimeSettingsUtil() {
+        return runtimeSettingsUtil;
+    }
+
+    @ModelAttribute("messageSourceUtil")
+    public MessageSourceUtil messageSourceUtil() {
+        return messageSourceUtil;
+    }
+
+    @ModelAttribute("teamUtil")
+    public TeamUtil teamUtil() {
+        return teamUtil;
+    }
+
+    @ModelAttribute("groupAvailabilityUtil")
+    public GroupAvailabilityUtil groupAvailabilityUtil() {
+        return groupAvailabilityUtil;
+    }
+
+    @ModelAttribute("languageIconUtil")
+    public LanguageIconUtil languageIconUtil() {
+        return languageIconUtil;
+    }
+
+    @ModelAttribute("languageUtil")
+    public LanguageUtil languageUtil() {
+        return languageUtil;
+    }
+
+    @ModelAttribute("bettingWebUtil")
+    public BettingWebUtil bettingWebUtil() {
+        return bettingWebUtil;
+    }
+}
